5f2b9259689762e6943478dca6c8b64ce459f0ae
Sébastien Bénard <github@deepnight.net>
2020-08-28 10:24:11 +0200

    Fixes & doc

--- src/dn/Changelog.hx
+++ src/dn/Changelog.hx
@@ -5,60 +5,133 @@ package dn;
 // See: https://semver.org/
 
 typedef ChangelogEntry = {
+	/**
+		Version with format x[.y][.z][-label]
+	**/
+	var shortVersion: String;
+
+	/**
+		Version with format x.y.z[-label]
+	**/
 	var fullVersion: String;
-	var major: Int;
+
+
+	/**
+		"x" in "x.y.z-label"
+	**/
+	var major: Int; // x
+
+	/**
+		"y" in "x.y.z-label"
+	**/
 	var minor: Null<Int>;
+
+	/**
+		"z" in "x.y.z-label"
+	**/
 	var patch: Null<Int>;
+
+	/**
+		"label" in "x.y.z-label"
+	**/
 	var metaLabel: Null<String>;
 
+	/**
+		Version title
+	**/
 	var title: Null<String>;
+
+	/**
+		Markdown description lines
+	**/
 	var linesMd : Array<String>;
 }
 
+
 class Changelog {
-	static var VERSION_TITLE_REG = ~/^[ \t]*##[ \t]+([0-9.]+)[ \t\-]*(.*)$/gim;
+	/**
+		Customizable title parser to recognize lines with a new version number
+	**/
+	public static var VERSION_TITLE_REG = ~/^[ \t]*##[ \t]+([0-9.a-z\-]+)[\- \t]*(.*)$/gim;
+
 	static var VERSION_NUMBER_REG = ~/^([0-9]+)[.]*([0-9a-z]*)[.]*([0-9]*)\-*([a-z0-9]*)/gi;
 
+	/**
+		Changelog entries
+	**/
 	public var versions : Array<ChangelogEntry>;
 
+
+	/**
+		Version numbers should comply to the SemVer format.
+		See parse() for more informations.
+	**/
 	public function new(markdown:String) {
 		parse(markdown);
 	}
 
+	@:keep
+	public function toString() {
+		return 'Changelog, ${versions.length} entries = ['
+			+ versions.map(function(v) {
+				return '${v.fullVersion}, "${v.title}", ${v.linesMd.length} lines';
+			}).join("], [")
+			+ "]";
+	}
+
+	public static inline function fromString(markdown:String) {
+		return new Changelog(markdown);
+	}
+
+	function getVersion(v:ChangelogEntry, fillZeros=false) : String {
+		return v.major
+		+ ( v.minor!=null ? "."+v.minor : fillZeros ? ".0" : "" )
+		+ ( v.patch!=null ? "."+v.patch : fillZeros ? ".0" : "" )
+		+ ( v.metaLabel!=null ? "-"+v.metaLabel : "" );
+	}
+
+	/**
+		Version numbers should comply to the SemVer format.
+
+		Changelog markdown should comply to the example below. Parser can be customized by
+		changing static RegEx.
+
+		\#\# 0.1 - Some title
+
+		hello world
+
+		foo
+
+		\#\# 0.0.1-alpha - Another title
+
+		bar
+	**/
 	public function parse(markdown:String) {
 		versions = [];
 
 		var lines = markdown.split("\n");
 		var cur  : ChangelogEntry = null;
 		for(l in lines) {
-			// l = StringTools.trim(l);
-			// if( l.length==0 )
-			// 	continue;
-
 			if( VERSION_TITLE_REG.match(l) ) {
+				var rawVersion = VERSION_TITLE_REG.matched(1);
+
+				// Parse version number according to SemVer format
+				var r = VERSION_NUMBER_REG;
+				if( !r.match(rawVersion) )
+					throw "Version number "+rawVersion+" in changelog doesn't comply to SemVer semantics";
+
 				cur = {
-					fullVersion: VERSION_TITLE_REG.matched(1),
-					major: -1,
-					minor: -1,
-					patch: -1,
-					metaLabel: null,
-					title: VERSION_TITLE_REG.matched(2),
+					fullVersion: "???",
+					shortVersion: "???",
+					major: Std.parseInt( r.matched(1) ),
+					minor: r.matched(2)!="" ? Std.parseInt( r.matched(2) ) : null,
+					patch: r.matched(3)!="" ? Std.parseInt( r.matched(3) ) : null,
+					metaLabel: r.matched(4)!="" ?r.matched(4) : null,
+					title: VERSION_TITLE_REG.matched(2)=="" ? null : VERSION_TITLE_REG.matched(2),
 					linesMd: [],
 				}
-
-				if( !VERSION_NUMBER_REG.match(cur.fullVersion) )
-					throw "Version number "+cur.fullVersion+" in changelog doesn't comply to SemVer semantics";
-
-				cur.major = Std.parseInt( VERSION_NUMBER_REG.matched(1) );
-
-				if( VERSION_NUMBER_REG.matched(2)!=null )
-					cur.minor = Std.parseInt( VERSION_NUMBER_REG.matched(2) );
-
-				if( VERSION_NUMBER_REG.matched(3)!=null )
-					cur.patch = Std.parseInt( VERSION_NUMBER_REG.matched(3) );
-
-				if( VERSION_NUMBER_REG.matched(4)!=null )
-					cur.metaLabel = VERSION_NUMBER_REG.matched(4);
+				cur.fullVersion = getVersion(cur, true);
+				cur.shortVersion = getVersion(cur, false);
 
 				versions.push(cur);
 				continue;
