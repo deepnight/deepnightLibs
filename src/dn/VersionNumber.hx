4ae24add7210d55d3ed0c223f1471c869ac63ac3
Sébastien Bénard <github@deepnight.net>
2020-09-18 12:30:27 +0200

    Updated VersionNumber

--- src/dn/VersionNumber.hx
+++ src/dn/VersionNumber.hx
@@ -1,35 +1,60 @@
 package dn;
 
+// Version numbers should follow SemVer semantic x.y.z-label
+// Everything except x is optional
+// See: https://semver.org/
+
 class VersionNumber {
-	static var VERSION_REG = ~/^([0-9]+)[.]*([0-9a-z]*)[.]*([0-9]*)\-*([a-z0-9]*)/gi;
+	static var VERSION_REG = ~/^[ \t]*([0-9]+)[.]*([0-9a-z]*)[.]*([0-9]*)\-*([a-z0-9]*)/gim;
 
 	public var major = 0;
 	public var minor = 0;
 	public var patch = 0;
-	public var tag : Null<String>;
+	public var label : Null<String>;
 
+	/** Version string in format "x.y.z" (without label)**/
+	public var numbers(get,never) : String;
+		inline function get_numbers() return '$major.$minor.$patch';
+
+	/** Version string in format "x.y.z[-label]"" **/
+	public var full(get,never) : String;
+		inline function get_full() return '$numbers${ label!=null ? "-"+label : "" }';
+
+
+	/**
+		"v" should be a version number using "x.y.z[-label]" format.
+	**/
 	public function new(?v:String) {
+		if( v!=null )
+			set(v);
+	}
+
+	/**
+		Set version using "x.y.z[-label]" format.
+	**/
+	public function set(v:String) {
 		if( v!=null && VERSION_REG.match(v) ) {
 			major = Std.parseInt( VERSION_REG.matched(1) );
 			minor = VERSION_REG.matched(2)=="" ? 0 : Std.parseInt( VERSION_REG.matched(2) );
 			patch = VERSION_REG.matched(3)=="" ? 0 : Std.parseInt( VERSION_REG.matched(3) );
-			tag = VERSION_REG.matched(4)=="" ? null : VERSION_REG.matched(4);
+			label = VERSION_REG.matched(4)=="" ? null : VERSION_REG.matched(4);
 		}
 	}
 
+	@:keep
+	public function toString() {
+		return full;
+	}
 
 
 	/**
-		Return 0 if both versions are equal, 1 if greater than "with", -1 if lower than "with"
+		Similar to Reflect.compare().
 
-		tag is ignored
+		Return 0 if versions are equal, 1 if this is greater than "with", -1 if this is lower than "with"
 	**/
 	public function compare(?with:VersionNumber, ?withStr:String) {
 		if( with==null )
 			with = new VersionNumber(withStr);
-
-		// if( major==with.major && minor==with.minor && patch==with.patch )
-		// 	return 0;
 
 		if( major!=with.major )
 			return Reflect.compare(major, with.major);
@@ -37,6 +62,8 @@ class VersionNumber {
 			return Reflect.compare(minor, with.minor);
 		else if( patch!=with.patch )
 			return Reflect.compare(patch, with.patch);
+		else if( label!=with.label)
+			return Reflect.compare(label, with.label);
 		else
 			return 0;
 	}
@@ -59,19 +86,11 @@ class VersionNumber {
 
 
 	/**
-		Return TRUE is both versions are equal (NOTE: tag is ignored)
+		Return TRUE is both versions are equal (NOTE: label is ignored)
 	**/
 	public static inline function isEqual(cur:String, than:String) : Bool return {
 		return new VersionNumber(cur).compare( new VersionNumber(than) ) == 0;
 	}
-
-
-	@:keep
-	public function toString() {
-		trace(major+" "+minor+" "+patch+" "+tag);
-		return'$major.$minor.$patch${ tag!=null ? "-"+tag : "" }';
-	}
-
 
 	@:noCompletion
 	public static function __test() {
@@ -82,28 +101,27 @@ class VersionNumber {
 		CiAssert.isTrue( new VersionNumber("10.02.30").major==10 );
 		CiAssert.isTrue( new VersionNumber("10.02.30").minor==2 );
 		CiAssert.isTrue( new VersionNumber("10.02.30").patch==30 );
+		CiAssert.isTrue( new VersionNumber("   1.2.3").major==1 );
 		CiAssert.isTrue( new VersionNumber("1").minor==0 );
 		CiAssert.isTrue( new VersionNumber("1.2").patch==0 );
 		CiAssert.isTrue( new VersionNumber("1.0.6").patch==6 );
-		CiAssert.isTrue( new VersionNumber("1.0.6-someTag").patch==6 );
-		CiAssert.isTrue( new VersionNumber("1.0.6-someTag").tag=="someTag" );
-		CiAssert.isTrue( new VersionNumber("1.0.6-some tag").tag=="some" );
+		CiAssert.isTrue( new VersionNumber("1.0.6-someLabel").patch == 6 );
+		CiAssert.isTrue( new VersionNumber("1.0.6-someLabel").label == "someLabel" );
+		CiAssert.isTrue( new VersionNumber("1.0.6-some tag").label == "some" );
+		CiAssert.isTrue( new VersionNumber("   1.2.3  ").full=="1.2.3" );
+		CiAssert.isTrue( new VersionNumber("1.2.3-alpha").numbers=="1.2.3" );
 
-		var a = new VersionNumber("1.0.2");
-		var b = new VersionNumber("0.10.2-alpha");
-		var c = new VersionNumber("1.0.3-rc1");
-
-		CiAssert.isNotNull(a);
-		CiAssert.isNotNull(b);
-		CiAssert.isNotNull(c);
-
-		CiAssert.isTrue( a.compare(b)==1 );
-		CiAssert.isTrue( a.compare(c)==-1 );
-		CiAssert.isTrue( b.compare(a)==-1 );
+		CiAssert.isTrue( new VersionNumber("1.0.1").compare( new VersionNumber("1.0.1") ) == 0 );
+		CiAssert.isTrue( new VersionNumber("0.0.2").compare( new VersionNumber("0.0.1") ) == 1 );
+		CiAssert.isTrue( new VersionNumber("0.0.2").compare( new VersionNumber("0.0.3") ) == -1 );
+		CiAssert.isTrue( new VersionNumber("1.0.1").compare( new VersionNumber("1.0.2") ) == -1 );
+		CiAssert.isTrue( new VersionNumber("1.0.1-a").compare( new VersionNumber("1.0.0-b") ) == 1 );
+		CiAssert.isTrue( new VersionNumber("1.0.1-a").compare( new VersionNumber("1.0.1-b") ) == -1 );
 
 		CiAssert.isTrue( VersionNumber.isGreater("1.0.5", "0.9.9-alpha") );
 		CiAssert.isTrue( VersionNumber.isLower("1.0.5", "1.1.0-alpha") );
 		CiAssert.isTrue( VersionNumber.isEqual("1.0.5", "1.0.5") );
-		CiAssert.isTrue( VersionNumber.isEqual("1.0", "1.0-alpha") );
+		CiAssert.isFalse( VersionNumber.isEqual("1.0", "1.0-alpha") );
+		CiAssert.isTrue( VersionNumber.isEqual("1.0.5-a", "1.0.5-a") );
 	}
 }
