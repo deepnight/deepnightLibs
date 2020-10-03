e7117a4609e897e021524c0a4af14fc8d58f6f28
Sébastien Bénard <github@deepnight.net>
2020-10-03 14:12:00 +0200

    Added knockout option to pixel outline filter

--- src/dn/heaps/filter/PixelOutline.hx
+++ src/dn/heaps/filter/PixelOutline.hx
@@ -3,18 +3,26 @@ package dn.heaps.filter;
 // --- Filter -------------------------------------------------------------------------------
 class PixelOutline extends h2d.filter.Filter {
 	public var color(default, set) : Int;
+	public var knockOut(default,set) : Bool;
 	var pass : PixelOutlinePass;
 
-	public function new(color=0x0) {
+	public function new(color=0x0, knockOut=false) {
 		super();
 		pass = new PixelOutlinePass(color);
 		this.color = color;
 		smooth = false;
+		this.knockOut = knockOut;
 	}
 
 	inline function set_color(v:Int) {
 		color = v;
 		pass.color = color;
+		return v;
+	}
+
+	inline function set_knockOut(v) {
+		knockOut = v;
+		pass.knockOut = knockOut;
 		return v;
 	}
 
@@ -34,10 +42,12 @@ class PixelOutline extends h2d.filter.Filter {
 @ignore("shader")
 private class PixelOutlinePass extends h3d.pass.ScreenFx<PixelOutlineShader> {
 	public var color(default, set) : Int;
+	public var knockOut(default, set) : Bool;
 
 	public function new(color=0x0) {
 		super(new PixelOutlineShader());
 		this.color = color;
+		knockOut = false;
 	}
 
 	function set_color(c) {
@@ -46,12 +56,19 @@ private class PixelOutlinePass extends h3d.pass.ScreenFx<PixelOutlineShader> {
 		return color = c;
 	}
 
+	function set_knockOut(v) {
+		if( knockOut==v )
+			return v;
+		return knockOut = v;
+	}
+
 	public function apply(src:h3d.mat.Texture, out:h3d.mat.Texture) {
 		engine.pushTarget(out);
 
 		shader.texture = src;
 		shader.outlineColor.setColor(color);
 		shader.pixelSize.set(1/src.width, 1/src.height);
+		shader.knockOut = knockOut?1:0;
 		render();
 
 		engine.popTarget();
@@ -65,6 +82,7 @@ private class PixelOutlineShader extends h3d.shader.ScreenShader {
 		@param var texture : Sampler2D;
 		@param var pixelSize : Vec2;
 		@param var outlineColor : Vec3;
+		@param var knockOut : Int;
 
 		function fragment() {
 			var curColor : Vec4 = texture.get(input.uv);
@@ -74,10 +92,10 @@ private class PixelOutlineShader extends h3d.shader.ScreenShader {
 				 || texture.get( vec2(input.uv.x, input.uv.y+pixelSize.y) ).a!=0
 				 || texture.get( vec2(input.uv.x, input.uv.y-pixelSize.y) ).a!=0 )
 					output.color = vec4(outlineColor, 1);
-				else
+				else if( knockOut==0 )
 					output.color = curColor;
 			}
-			else
+			else if( knockOut==0 )
 				output.color = curColor;
 		}
 	};
