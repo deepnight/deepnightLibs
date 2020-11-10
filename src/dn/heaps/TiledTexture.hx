39c8a30e0236557e93511bfdd2ee5b6519b5dde7
Sébastien Bénard <github@deepnight.net>
2020-11-10 12:12:21 +0100

    Added support for resizing to TiledTexture

--- src/dn/heaps/TiledTexture.hx
+++ src/dn/heaps/TiledTexture.hx
@@ -3,15 +3,21 @@ package dn.heaps;
 class TiledTexture extends h2d.TileGroup {
 	public function new(texTile:h2d.Tile, wid:Int, hei:Int, ?p:h2d.Object) {
 		super(texTile, p);
+		resize(wid,hei);
+	}
+
+	public function resize(wid:Int,hei:Int) {
+		clear();
 		var x = 0;
 		var y = 0;
 		while( y<hei) {
-			add( x, y, texTile.sub( 0, 0, M.fmin(wid-x,texTile.width), M.fmin(hei-y,texTile.height) ) );
-			x += Std.int(texTile.width);
+			add( x, y, tile.sub( 0, 0, M.fmin(wid-x,tile.width), M.fmin(hei-y,tile.height) ) );
+			x += Std.int(tile.width);
 			if( x>=wid ) {
 				x = 0;
-				y += Std.int(texTile.height);
+				y += Std.int(tile.height);
 			}
 		}
+
 	}
 }
