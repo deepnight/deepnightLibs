13d7d6e6254dd7fb58f17c5b4aa12fbe9bfa2a12
Sébastien Bénard <github@deepnight.net>
2021-01-06 13:08:21 +0100

    Tests cleanup

--- tests/Main.hx
+++ tests/Main.hx
@@ -1,4 +1,6 @@
-import dn.*;
+import dn.CiAssert;
+import dn.Lib;
+
 class Main {
 	#if !macro
 	public static function main() {
@@ -8,24 +10,25 @@ class Main {
 		CiAssert.isFalse(false);
 		CiAssert.isNotNull("a");
 
-		Cinematic.__test();
-		Cooldown.__test();
+		// Libs
+		dn.Lib.__test();
+		dn.Cinematic.__test();
+		dn.Cooldown.__test();
 		dn.data.GetText.__test();
-		DecisionHelper.__test();
-		Delayer.__test();
-		Rand.__test();
-		RandList.__test();
-		RandDeck.__test();
-		Color.__test();
-		Bresenham.__test();
-		FilePath.__test();
+		dn.DecisionHelper.__test();
+		dn.Delayer.__test();
+		dn.Rand.__test();
+		dn.RandList.__test();
+		dn.RandDeck.__test();
+		dn.Color.__test();
+		dn.Bresenham.__test();
+		dn.FilePath.__test();
 		dn.pathfinder.AStar.__test();
-		Version.__test();
-		M.__test();
-		Changelog.__test();
-		LocalStorage.__test();
-		Lib.__test();
-		Args.__test();
+		dn.Version.__test();
+		dn.M.__test();
+		dn.Changelog.__test();
+		dn.LocalStorage.__test();
+		dn.Args.__test();
 
 		// Done!
 		Lib.println("");
