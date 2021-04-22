6886ea0ae8faa4f94670535223368c3295675852
Sébastien Benard <github@deepnight.net>
2021-04-22 10:16:47 +0200

    Added Aseprite identifying

--- src/dn/Identify.hx
+++ src/dn/Identify.hx
@@ -10,10 +10,13 @@ enum IdentifyFormat {
 	Jpeg;
 	Gif;
 	Bmp;
+	Aseprite;
 }
 
+private typedef FileHeader = { id:IdentifyFormat, ?skipBytes:Int, magic:Array<Int> };
+
 class Identify {
-	static var formats = [
+	static var headers : Array<FileHeader> = [
 		// Note: "-1" means "any byte"
 		{ id:Png,  magic:[0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A] },
 
@@ -25,16 +28,18 @@ class Identify {
 		{ id:Jpeg, magic:[0xFF,0xD8,0xFF,0xE0,-1,-1,0x4A,0x46,0x49,0x46,0x00,0x01] }, // Jpeg JFIF
 		{ id:Jpeg, magic:[0xFF,0xD8,0xFF,0xE1,-1,-1,0x45,0x78,0x69,0x66,0x00,0x00] }, // Jpeg EXIF
 
-		{ id:Bmp,  magic:[0x42,0x4d] },
+		{ id:Aseprite, skipBytes:4, magic:[0xE0,0xA5] },
+
+		{ id:Bmp, magic:[0x42,0x4d] },
 	];
 
 	public static function getType(b:haxe.io.Bytes) : IdentifyFormat {
 		if( b==null )
 			return Unknown;
 
-		for(f in formats)
-			if( matchHeader(b, f.magic) )
-				return f.id;
+		for(h in headers)
+			if( matchHeader(b, h) )
+				return h.id;
 
 		return Unknown;
 	}
@@ -55,9 +60,9 @@ class Identify {
 	}
 
 
-	static function matchHeader(b:haxe.io.Bytes, magicNumbers:Array<Int>) {
-		for( idx in 0...magicNumbers.length )
-			if( idx>=b.length || magicNumbers[idx]!=b.get(idx) && magicNumbers[idx]>=0 )
+	static function matchHeader(b:haxe.io.Bytes, h:FileHeader) {
+		for( i in 0...h.magic.length )
+			if( i+h.skipBytes>=b.length  ||  h.magic[i]>=0  &&  h.magic[i] != b.get(i+h.skipBytes) )
 				return false;
 
 		return true;
