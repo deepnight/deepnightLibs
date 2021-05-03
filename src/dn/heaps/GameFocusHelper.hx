60308704408a17a5626cdffee20571181840761d
Sébastien Benard <github@deepnight.net>
2021-05-03 10:22:54 +0200

    Added cover art support

--- src/dn/heaps/GameFocusHelper.hx
+++ src/dn/heaps/GameFocusHelper.hx
@@ -8,10 +8,12 @@ class GameFocusHelper extends dn.Process {
     var font : h2d.Font;
     var scene : h2d.Scene;
     var showIntro = false;
+    var thumb : h2d.Tile;
 
-    public function new(s:h2d.Scene, font:h2d.Font) {
+    public function new(s:h2d.Scene, font:h2d.Font, ?thumb:h2d.Tile) {
         super();
 
+        this.thumb = thumb;
         this.font = font;
         this.scene = s;
         createRoot(scene);
@@ -21,8 +23,10 @@ class GameFocusHelper extends dn.Process {
         showIntro = true;
         suspendGame();
         #else
-        if( !isFocused() )
+        if( !isFocused() ) {
+            showIntro = true;
             suspendGame();
+        }
         #end
 
         // #if js
@@ -63,7 +67,13 @@ class GameFocusHelper extends dn.Process {
         root.visible = true;
         root.removeChildren();
 
-        var bg = new h2d.Bitmap( h2d.Tile.fromColor(showIntro?0x252e43:0x0, 1, 1, showIntro?1:0.6), root );
+        var isThumb = showIntro && thumb!=null;
+        var t = showIntro && thumb==null
+            ? h2d.Tile.fromColor(0x252e43, 1,1, 1)
+            : showIntro && thumb!=null
+                ? thumb
+                : h2d.Tile.fromColor(0x0, 1,1, 0.6);
+        var bg = new h2d.Bitmap(t, root);
         var i = new h2d.Interactive(1,1, root);
 
         var tf = new h2d.Text(font, root);
@@ -75,14 +85,23 @@ class GameFocusHelper extends dn.Process {
         createChildProcess(
             function(c) {
                 // Resize dynamically
-                tf.setScale( M.imax(1, Math.floor( w()*0.35 / tf.textWidth )) );
+                tf.setScale( M.imax(1, Math.floor( w()*0.5 / tf.textWidth )) );
                 tf.x = Std.int( w()*0.5 - tf.textWidth*tf.scaleX*0.5 );
                 tf.y = Std.int( h()*0.5 - tf.textHeight*tf.scaleY*0.5 );
 
                 i.width = w()+1;
                 i.height = h()+1;
-                bg.scaleX = w()+1;
-                bg.scaleY = h()+1;
+                if( isThumb ) {
+                    var s = M.fmax( w()/thumb.width, h()/thumb.height );
+                    bg.filter = new h2d.filter.Blur(32,1,3);
+                    bg.setScale(s);
+                    bg.x = w()*0.5 - bg.tile.width*bg.scaleX*0.5;
+                    bg.y = h()*0.5 - bg.tile.height*bg.scaleY*0.5;
+                }
+                else {
+                    bg.scaleX = w()+1;
+                    bg.scaleY = h()+1;
+                }
 
                 // Auto-kill
                 if( !suspended )
