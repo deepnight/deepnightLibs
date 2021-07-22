fc7eef8f7fbc1f730ea214c791aaf9558e6713dc
Marcel Müller <com2040@gmail.com>
2021-07-22 06:50:50 +0000

    Document Bresenham.hx (#25)
    * Document Bresenham.hx
    
    * Move private method to the bottom

--- src/dn/Bresenham.hx
+++ src/dn/Bresenham.hx
@@ -1,19 +1,29 @@
 package dn;
 
+/**
+ * Various helper methods for 2D grid traversal.
+ * 
+ * There's two kinds of line helpers:
+ *   - `Thin`, meaning that the line does diagonal jumps
+ *   - `Thick`, meaning that the line does *not* do diagonal jumps
+ * 
+ * There's also two kinds of circular helpers:
+ *   - `Circle`, meaning only the edge is taken into account
+ *   - `Disc`, meaning the entirety of a disc is taken into account
+ */
 class Bresenham {
+
 	/**
-		The "Thin" methods work on regular "pixel perfect" bresenhman lines, like:
-			###
-			   ###
-			      ###
-
-	  The "Thick" versions are slightly thicker when getting pixels in diagonal:
-			###
-			  ####
-			     ####
-
-	**/
-
+	 * Get a list of coordinates from `(x0, y0)` to `(x1, y1)`, where diagonal jumps are allowed.
+	 * 
+	 * If you do not want diagonal jumps use `getThickLine` instead.
+	 * @param x0 Starting x coordinate
+	 * @param y0 Starting y coordinate
+	 * @param x1 End x coordinate
+	 * @param y1 End y coordinate
+	 * @param respectOrder=false Whether the list of points should be ordered start -> end. (This might not be the case when the end is geometrically closer to the origin than the start)
+	 * @return Array<{x:Int, y:Int}> An array of points along the line
+	 */
 	public static function getThinLine(x0:Int, y0:Int, x1:Int, y1:Int, ?respectOrder=false) : Array<{x:Int, y:Int}> {
 		var pts = [];
 		var swapXY = M.iabs( y1 - y0 ) > M.iabs( x1 - x0 );
@@ -62,14 +72,24 @@ class Bresenham {
 		return pts;
 	}
 
-
-
 	@:noCompletion
 	@:deprecated("Use getThickLine() instead")
 	public static inline function getFatLine(x0:Int, y0:Int, x1:Int, y1:Int, ?respectOrder=false) {
 		return getThickLine(x0,y0,x1,y1,respectOrder);
 	}
-	public static function getThickLine(x0:Int, y0:Int, x1:Int, y1:Int, ?respectOrder=false) {
+
+	/**
+	 * Get a list of coordinates from `(x0, y0)` to `(x1, y1)`, without diagonal jumps.
+	 * 
+	 * If you do want diagonal jumps use `getThickLine` instead.
+	 * @param x0 Starting x coordinate
+	 * @param y0 Starting y coordinate
+	 * @param x1 End x coordinate
+	 * @param y1 End y coordinate
+	 * @param respectOrder=false Whether the list of points should be ordered start -> end. (This might not be the case when the end is geometrically closer to the origin than the start)
+	 * @return Array<{x:Int, y:Int}> An array of points along the line
+	 */
+	public static function getThickLine(x0:Int, y0:Int, x1:Int, y1:Int, ?respectOrder=false): Array<{x:Int, y:Int}> {
 		var pts = [];
 		var swapXY = M.iabs( y1 - y0 ) > M.iabs( x1 - x0 );
 		var swapped = false;
@@ -131,26 +151,47 @@ class Bresenham {
 
 	// Source : http://en.wikipedia.org/wiki/Midpoint_circle_algorithm
 	// Duplicates fix: https://stackoverflow.com/questions/44117168/midpoint-circle-without-duplicates
-	public inline static function getCircle(x0,y0,radius) {
+	/**
+	 * Get the points along the edge of a circle at `(x0, y0)` with `radius`.
+	 * 
+	 * If you want the points inside the circle as well, check out `getDisc`.
+	 * @param x0 Center x coordinate
+	 * @param y0 Center y coordinate
+	 * @param radius Radius
+	 * @return Array<{x:Int, y:Int}> An array of points along the circle
+	 */
+	public inline static function getCircle(x0,y0,radius):Array<{x:Int, y:Int}> {
 		var pts = [];
 		iterateCircle(x0, y0, radius, function(x,y) pts.push( { x:x, y:y } ));
 		return pts;
 	}
 
-
-
 	// Source : http://stackoverflow.com/questions/1201200/fast-algorithm-for-drawing-filled-circles
-	public static function getDisc(x0,y0,radius) {
+	/**
+	 * Get the points inside a disc at `(x0, y0)` with `radius`.
+	 * 
+	 * If you only want the border, check out `getDisc`.
+	 * @param x0 Center x coordinate
+	 * @param y0 Center y coordinate
+	 * @param radius Radius
+	 * @return Array<{x:Int, y:Int}> An array of points filling the disc
+	 */
+	public static function getDisc(x0,y0,radius):Array<{x:Int, y:Int}> {
 		var pts = [];
 		iterateDisc(x0, y0, radius, function(x,y) pts.push( { x:x, y:y } ));
 		return pts;
 	}
 
-	static inline function _iterateHorizontal(fx,fy, tx, cb:Int->Int->Void) {
-		for(x in fx...tx+1)
-			cb(x,fy);
-	}
-
+	/**
+	 * A helper function to iterate over all integer tiles of a disc and call a callback
+	 * for each one of them.
+	 * 
+	 * If you only want to iterate the border, check out `iterateCircle`.
+	 * @param x0 Center x coordinate
+	 * @param y0 Center y coordinate
+	 * @param radius Radius
+	 * @param cb a callback, called for each tile of the disc
+	 */
 	public static function iterateDisc(x0,y0,radius, cb:Int->Int->Void) {
 		var x = radius;
 		var y = 0;
@@ -177,7 +218,16 @@ class Bresenham {
 		}
 	}
 
-
+	/**
+	 * A helper function to iterate over all integer tiles of the border of a circle and call a callback
+	 * for each one of them.
+	 * 
+	 * If you want to iterate the whole disc, check out `iterateDisc`.
+	 * @param x0 Center x coordinate
+	 * @param y0 Center y coordinate
+	 * @param radius Radius
+	 * @param cb callback, executed for each tile visited
+	 */
 	public static function iterateCircle(x0,y0,radius, cb:Int->Int->Void) {
 		var x = radius;
 		var y = 0;
@@ -214,6 +264,18 @@ class Bresenham {
 	}
 
 
+	/**
+	 * A helper function to iterate over each tile in a line from `(x0, y0)` to `(x1, y1)` while
+	 * allowing diagonal traversal.
+	 * 
+	 * If you do not want to allow diagonals, check out `iterateThickLine`.
+	 * If you want to have the list of points instead, check out `getThinLine`.
+	 * @param x0 Start x coordinate
+	 * @param y0 Start y coordinate
+	 * @param x1 End x coordinate
+	 * @param y1 End y coordinate
+	 * @param cb Callback, executed for each tile visited
+	 */
 	public inline static function iterateThinLine(x0:Int,y0:Int, x1:Int,y1:Int, cb:Int->Int->Void) {
 		var swapXY = M.iabs( y1 - y0 ) > M.iabs( x1 - x0 );
 		var tmp : Int;
@@ -246,7 +308,18 @@ class Bresenham {
 		}
 	}
 
-
+	/**
+	 * A helper function to iterate over each tile in a line from `(x0, y0)` to `(x1, y1)` 
+	 * without diagonal traversal.
+	 * 
+	 * If you do want allow diagonals, check out `iterateThinLine`.
+	 * If you want to have the list of points instead, check out `getThickLine`.
+	 * @param x0 Start x coordinate
+	 * @param y0 Start y coordinate
+	 * @param x1 End x coordinate
+	 * @param y1 End y coordinate
+	 * @param cb Callback, executed for each tile visited
+	 */
 	public inline static function iterateThickLine(x0:Int,y0:Int, x1:Int,y1:Int, cb:Int->Int->Void) {
 		var swapXY = M.iabs( y1 - y0 ) > M.iabs( x1 - x0 );
 		var tmp : Int;
@@ -287,9 +360,21 @@ class Bresenham {
 
 
 
-	// Vérifie que tous les points d'une ligne sont valides: si rayCanPass(x,y) renvoie FALSE sur
-	// un point (ie. obstacle), toute la méthode renvoie FALSE
-	public inline static function checkThinLine(x0:Int,y0:Int, x1:Int,y1:Int, rayCanPass:Int->Int->Bool) {
+	/**
+	 * Check whether for a given line, a condition holds. Returns `false` if at any time the condition fails. Otherwise, returns `true`.
+	 * 
+	 * This can be used to check for line of sight for example, or wether the line intersects a specific tile.
+	 * 
+	 * This function does diagonal jumps, if you do not want those, see `checkThickLine`.
+	 * 
+	 * @param x0 Start x coordinate
+	 * @param y0 Start y coordinate
+	 * @param x1 End x coordinate
+	 * @param y1 End y coordinate
+	 * @param rayCanPass A condition checked for each tile. If this returns `false`, the whole function returns `false`.
+	 * @return Bool Only `true` if for the whole line `rayCanPass` returned `true` as well, otherwise `false`.
+	 */
+	public inline static function checkThinLine(x0:Int,y0:Int, x1:Int,y1:Int, rayCanPass:Int->Int->Bool):Bool {
 		if( !rayCanPass(x0,y0) || !rayCanPass(x1,y1) ){
 			return false;
 		}else{
@@ -333,7 +418,22 @@ class Bresenham {
 	public static inline function checkFatLine(x0:Int,y0:Int, x1:Int,y1:Int, rayCanPass:Int->Int->Bool) {
 		return checkThickLine(x0,y0,x1,y1,rayCanPass);
 	}
-	public inline static function checkThickLine(x0:Int,y0:Int, x1:Int,y1:Int, rayCanPass:Int->Int->Bool) {
+
+	/**
+	 * Check whether for a given line, a condition holds. Returns `false` if at any time the condition fails. Otherwise, returns `true`.
+	 * 
+	 * This can be used to check for line of sight for example, or wether the line intersects a specific tile.
+	 * 
+	 * This function does not do diagonal jumps, if you do want those, see `checkThinLine`.
+	 * 
+	 * @param x0 Start x coordinate
+	 * @param y0 Start y coordinate
+	 * @param x1 End x coordinate
+	 * @param y1 End y coordinate
+	 * @param rayCanPass A condition checked for each tile. If this returns `false`, the whole function returns `false`.
+	 * @return Bool Only `true` if for the whole line `rayCanPass` returned `true` as well, otherwise `false`.
+	 */
+	public inline static function checkThickLine(x0:Int,y0:Int, x1:Int,y1:Int, rayCanPass:Int->Int->Bool):Bool {
 		if( !rayCanPass(x0,y0) || !rayCanPass(x1,y1) ){
 			return false;
 		}else{
@@ -395,7 +495,11 @@ class Bresenham {
 			return valid;
 		}
 	}
-
+	
+	static inline function _iterateHorizontal(fx,fy, tx, cb:Int->Int->Void) {
+		for(x in fx...tx+1)
+			cb(x,fy);
+	}
 
 	@:noCompletion
 	public static function __test() {
