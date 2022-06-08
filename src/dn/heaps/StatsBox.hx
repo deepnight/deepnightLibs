d825117ea0785bdfa5d45d4ecee9c2bef0d4fe8e
Sebastien Benard <github@deepnight.net>
2022-06-08 11:54:39 +0200

    Updated StatsBox to use Chart

--- src/dn/heaps/StatsBox.hx
+++ src/dn/heaps/StatsBox.hx
@@ -1,24 +1,13 @@
 package dn.heaps;
 
 import hxd.Key as K;
+import dn.Col;
 
 /**
 	Stats box for Heaps (shows current FPS & FPS history chart).
 	Press ALT-S to hide temporarily.
 **/
-
 class StatsBox extends dn.Process {
-	static final HISTORY_CHUNK = 30;
-
-	/** Ideal FPS for the chart **/
-	public var targetFps = 60;
-
-	/** Frequency of updates in seconds **/
-	public var updateFrequencyS = 0.4;
-
-	/** Precision of the FPS counter **/
-	public var fpsPrecision = 0;
-
 	/** Screen X location of Stats (0=left -> 1=right) **/
 	public var anchorRatioX(default,set) : Float = 1;
 	inline function set_anchorRatioX(v) return anchorRatioX = M.fclamp(v,0,1);
@@ -27,27 +16,19 @@ class StatsBox extends dn.Process {
 	public var anchorRatioY(default,set) : Float = 1;
 		inline function set_anchorRatioY(v) return anchorRatioY = M.fclamp(v,0,1);
 
-	/** FPS chart width in pixels **/
-	public var chartWid = 70;
-
-	/** FPS chart height in pixels **/
-	public var chartHei = 20;
+	public var wid(default,set) : Int;
 
 	var anchor : h2d.col.Point;
 	var flow : h2d.Flow;
-	var fps : h2d.Text;
-	var engineInf : h2d.Text;
-	var fpsChart : h2d.Graphics;
+	public var updateFreqS(default,set) = 0.25;
 
 	var components : Array<{ f:h2d.Flow, update:(f:h2d.Flow)->Void}> = [];
-
-	var fpsHistory = new haxe.ds.Vector(HISTORY_CHUNK*3);
-	var histCursor = 0;
+	var charts : Array<Chart> = [];
 
 	public var font : Null<h2d.Font>;
 
 
-	public function new(parent:dn.Process, showFpsChart=true) {
+	public function new(parent:dn.Process) {
 		super(parent);
 
 		if( parent.root==null )
@@ -68,13 +49,24 @@ class StatsBox extends dn.Process {
 		var f = new h2d.Flow(flow);
 		f.layout = Horizontal;
 		f.verticalAlign = Middle;
-		fps = new h2d.Text(getFont(), f);
-		fpsChart = new h2d.Graphics(f);
-		fpsChart.visible = showFpsChart;
 
-		engineInf = new h2d.Text(getFont(), flow);
+		wid = 100;
+		Process.resizeAll();
+	}
 
-		onResize();
+
+	function set_wid(v) {
+		wid = v;
+		for(c in charts)
+			c.wid = wid;
+		return wid;
+	}
+
+	function set_updateFreqS(v) {
+		updateFreqS = v;
+		for(c in charts)
+			@:privateAccess c.freqS = v;
+		return wid;
 	}
 
 	inline function getFont() return font==null ? hxd.res.DefaultFont.get() : font;
@@ -96,15 +88,46 @@ class StatsBox extends dn.Process {
 		}});
 	}
 
+	public function addCustomChart( label="", ?color:Col, valueGetter:Void->Float, refValue=0.) : Chart {
+		var c = new Chart(label, color, getFont(), this);
+		flow.addChild(c.root);
+		c.wid = wid;
+		c.hei = 24;
+		c.refValue = refValue;
+		c.autoPlot( valueGetter, updateFreqS);
+		charts.push(c);
+		return c;
+	}
+
+	public function addFpsChart(refFps=60) {
+		addCustomChart("FPS", "#9ba3c1", ()->hxd.Timer.fps(), refFps);
+	}
+
+	#if heaps
+	public function addDrawCallsChart() {
+		var c = addCustomChart("DrawC", Red, ()->engine.drawCalls);
+	}
+	#end
+
+	#if hl
+	public function addMemoryChart() {
+		addCustomChart("MEM", Blue, ()->hl.Gc.stats().currentMemory);
+	}
+	#end
+
 	public function removeAllComponents() {
 		for(c in components)
 			c.f.remove();
 		components = [];
+
+		for(c in charts)
+			c.destroy();
+		charts = [];
 	}
 
 	override function onResize() {
 		super.onResize();
-		var s = Scaler.bestFit_i(chartWid, chartHei, Scaler.getViewportWidth()*0.1, Scaler.getViewportHeight()*0.1 );
+		var s = Scaler.bestFit_i(wid*6, 1);
 		root.setScale(s);
 	}
 
@@ -139,50 +162,5 @@ class StatsBox extends dn.Process {
 
 		if( K.isPressed(K.S) && K.isDown(K.ALT) )
 			toggle();
-
-		if( root.visible && !cd.hasSetS("tick",updateFrequencyS) ) {
-			var v = hxd.Timer.fps();
-
-			// Update FPS history
-			if( fpsChart.visible ) {
-				fpsHistory.set(histCursor++, v);
-				if( histCursor>=fpsHistory.length ) {
-					// History overflow
-					for( i in HISTORY_CHUNK...fpsHistory.length )
-						fpsHistory[i-HISTORY_CHUNK] = fpsHistory[i];
-					histCursor-=HISTORY_CHUNK;
-				}
-			}
-
-			// Display FPS
-			fps.text = Std.string( dn.M.pretty(v, fpsPrecision) );
-
-			// Engine infos
-			engineInf.text = "DC="+engine.drawCalls+"/Trg="+engine.drawTriangles;
-
-			// Draw chart
-			if( fpsChart.visible ) {
-				fpsChart.clear();
-				fpsChart.beginFill( dn.legacy.Color.makeColorRgb( 1, M.fclamp(v/targetFps, 0, 1), 0 ), 0.33 );
-				fpsChart.drawRect(0,0,chartWid,chartHei);
-				fpsChart.drawRect(0, chartHei * (1-targetFps/(targetFps*1.1)), chartWid, 1);
-				fpsChart.endFill();
-				fpsChart.lineStyle(1, 0xffffff);
-				for(i in 1...fpsHistory.length) {
-					if( i>=histCursor )
-						break;
-
-					fpsChart.lineStyle(1, dn.legacy.Color.makeColorRgb( 1, M.fclamp(fpsHistory[i-1]/targetFps, 0, 1), 0 ) );
-					fpsChart.moveTo(
-						chartWid * (i-1)/fpsHistory.length,
-						chartHei * (1-fpsHistory[i-1]/(targetFps*1.1))
-					);
-					fpsChart.lineTo(
-						chartWid*i/fpsHistory.length,
-						chartHei * (1-fpsHistory[i]/(targetFps*1.1))
-					);
-				}
-			}
-		}
 	}
 }
