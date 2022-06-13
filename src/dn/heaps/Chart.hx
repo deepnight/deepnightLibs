3cdbd503066445372b9710f36d26aca9e281f5c8
Sebastien Benard <github@deepnight.net>
2022-06-13 10:28:22 +0200

    Optimized Chart

--- src/dn/heaps/Chart.hx
+++ src/dn/heaps/Chart.hx
@@ -9,6 +9,7 @@ class Chart extends dn.Process {
 	public var hei(default,set) : Int;
 	public var color(default,set) : Col;
 	var max = 0.;
+	var avg = -1.;
 
 	/** Shows an horizontal line at this value **/
 	public var refValue(default,set) : Float = 0;
@@ -16,8 +17,10 @@ class Chart extends dn.Process {
 	/** Precision of the displayed number **/
 	public var precision = 1;
 
-	var g : h2d.Graphics;
-	var invalidated = true;
+	var pixels : h2d.SpriteBatch;
+	var pixelPool : haxe.ds.Vector<h2d.SpriteBatch.BatchElement>;
+	var chartInvalidated = true;
+	var baseInvalidated = true;
 	var showTexts = true;
 
 	var freqS = 0.5;
@@ -26,7 +29,7 @@ class Chart extends dn.Process {
 	var history : haxe.ds.Vector<Float>;
 	var curHistIdx = 0;
 	var label : String;
-	var last : h2d.Text;
+	var lastTf : h2d.Text;
 	var font : h2d.Font;
 
 	public function new(label="", color:Col=ColdLightGray, ?font:h2d.Font, p:Process) {
@@ -49,6 +52,7 @@ class Chart extends dn.Process {
 	/** Define the max number of values that will be displayed horizontally on the chart **/
 	public function setMaxSamples(n:Int) {
 		history = new haxe.ds.Vector(n);
+		pixelPool = new haxe.ds.Vector(n);
 		initBase();
 	}
 
@@ -62,31 +66,32 @@ class Chart extends dn.Process {
 
 	inline function set_color(c) {
 		color = c;
-		invalidated = true;
+		chartInvalidated = true;
 		return c;
 	}
 
 	inline function set_refValue(v) {
 		refValue = v;
-		invalidated = true;
+		chartInvalidated = true;
 		return v;
 	}
 
 	inline function set_wid(v) {
 		wid = v;
-		invalidated = true;
+		baseInvalidated = true;
 		return v;
 	}
 
 	inline function set_hei(v) {
 		hei = v;
-		invalidated = true;
+		baseInvalidated = true;
 		return v;
 	}
 
 	override function onDispose() {
 		super.onDispose();
-		g = null;
+		pixels = null;
+		pixelPool = null;
 		history = null;
 	}
 
@@ -104,37 +109,54 @@ class Chart extends dn.Process {
 			line.y = getY(refValue);
 		}
 
+		// Init pixels
+		var t = h2d.Tile.fromColor(color);
+		pixels = new h2d.SpriteBatch(t, root);
+		pixels.hasRotationScale = true;
+		var be = null;
+		for(i in 0...pixelPool.length) {
+			be = new h2d.SpriteBatch.BatchElement(t);
+			pixels.add(be);
+			be.visible = false;
+			pixelPool[i] = be;
+		}
+
 		if( showTexts ) {
 			var labelTf = new h2d.Text(font, root);
 			labelTf.text = label;
-			labelTf.textColor = color.toBlack(0.3);
+			labelTf.textColor = White;
 			labelTf.x = 4;
 			labelTf.y = hei-labelTf.textHeight-1;
 
-			last = new h2d.Text(font, root);
-			last.x = labelTf.x + labelTf.textWidth + 4;
-			last.textColor = color.toBlack(0.2);
+			lastTf = new h2d.Text(font, root);
+			lastTf.x = labelTf.x + labelTf.textWidth + 4;
+			lastTf.textColor = color.toWhite(0.85);
 			if( curHistIdx>0 )
 				printLast(history[curHistIdx-1]);
 		}
-
-		g = new h2d.Graphics(root);
 	}
 
 	inline function printLast(v:Float) {
 		if( showTexts ) {
-			last.text = Std.string( M.unit(v,precision) );
-			last.y = hei-last.textHeight-1;
+			lastTf.text = Std.string( M.unit(v,precision) );
+			lastTf.y = hei-lastTf.textHeight-1;
 		}
 	}
 
 	function renderFullChart() {
-		g.clear();
-		g.lineStyle(1, color);
-		g.moveTo( getX(0), getY(history[0]) );
-
-		for(i in 1...curHistIdx)
-			g.lineTo( getX(i), getY(history[i]) );
+		pixels.tile = h2d.Tile.fromColor(color);
+		var prev = null;
+		var cur = null;
+		var zero = getY(0);
+		for(i in 0...curHistIdx) {
+			cur = pixelPool[i];
+			cur.x = getX(i);
+			cur.y = getY(history[i]);
+			cur.scaleY = zero-cur.y;
+			cur.visible = true;
+		}
+		for(i in curHistIdx...pixelPool.length)
+			pixelPool[i].visible = false;
 	}
 
 	public function plot(v:Float) {
@@ -143,15 +165,11 @@ class Chart extends dn.Process {
 		history[curHistIdx] = v;
 
 		// Render
-		g.lineStyle(1, color);
-		if( curHistIdx==1 ) {
-			g.moveTo(getX(curHistIdx), getY(v));
-			g.lineTo(getX(curHistIdx), getY(v));
-		}
-		else {
-			g.moveTo(getX(curHistIdx-1), getY(history[curHistIdx-1]));
-			g.lineTo(getX(curHistIdx), getY(v));
-		}
+		var be = pixelPool[curHistIdx];
+		be.x = getX(curHistIdx);
+		be.y = getY(v);
+		be.visible = true;
+		be.scaleY = getY(0)-be.y;
 
 		printLast(v);
 		curHistIdx++;
@@ -159,6 +177,7 @@ class Chart extends dn.Process {
 		// Scroll back
 		if( curHistIdx>=history.length ) {
 			max = 0;
+			avg = -1;
 			var chunk = Std.int( history.length*0.33 );
 			for(i in chunk...history.length) {
 				history[i-chunk] = history[i];
@@ -167,15 +186,20 @@ class Chart extends dn.Process {
 
 			curHistIdx-=chunk;
 
-			invalidated = true;
+			chartInvalidated = true;
 		}
 	}
 
 	inline function updateMax(v:Float) {
-		var old = max;
-		max = M.fmax( max, M.fmax(v, refValue)* 1.1 ) ;
-		if( old!=max )
-			invalidated = true;
+		if( avg==-1 )
+			avg = v;
+		else
+			avg = (avg+v)*0.5;
+
+		if( avg*1.5>max || max>avg*3 ) {
+			max = avg*1.5;
+			chartInvalidated = true;
+		}
 	}
 
 	public function autoPlot( valueGetter:Void->Float, freqS:Float ) {
@@ -183,8 +207,8 @@ class Chart extends dn.Process {
 		autoPlotter = valueGetter;
 	}
 
-	inline function getX(idx:Int) return idx/history.length * wid;
-	inline function getY(v:Float) return hei - v/max*hei;
+	inline function getX(idx:Int) return M.round( idx/history.length * wid );
+	inline function getY(v:Float) return M.imax(0, M.round( hei - v/max*hei ));
 
 	override function update() {
 		super.update();
@@ -198,10 +222,15 @@ class Chart extends dn.Process {
 	override function postUpdate() {
 		super.postUpdate();
 
-		if( invalidated ) {
+		if( baseInvalidated) {
 			initBase();
+			chartInvalidated = true;
+			baseInvalidated = false;
+		}
+
+		if( chartInvalidated ) {
 			renderFullChart();
-			invalidated = false;
+			chartInvalidated = false;
 		}
 	}
 }
