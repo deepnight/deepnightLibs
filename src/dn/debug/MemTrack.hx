d4d648937b2f0f2e5190a911e325d82efaa8991b
Sebastien Benard <github@deepnight.net>
2022-06-15 17:32:44 +0200

    Better MemTrack reporting

--- src/dn/debug/MemTrack.hx
+++ src/dn/debug/MemTrack.hx
@@ -7,7 +7,9 @@ import haxe.macro.Context;
 
 class MemTrack {
 	@:noCompletion
-	public static var allocs : Map<String, { total:Float, time:Float, calls:Int }> = new Map();
+	public static var allocs : Map<String, { total:Float, calls:Int }> = new Map();
+
+	public static var firstMeasure = -1.;
 
 	/** Measure a block or a function call memory usage **/
 	public static macro function measure( e:Expr ) {
@@ -24,19 +26,18 @@ class MemTrack {
 		}
 
 		return macro {
-			var t = haxe.Timer.stamp();
+			if( dn.debug.MemTrack.firstMeasure<0 )
+				dn.debug.MemTrack.firstMeasure = haxe.Timer.stamp();
 			var old = hl.Gc.stats().currentMemory;
 
 			$e;
 
 			var m = dn.M.fmax( 0, hl.Gc.stats().currentMemory - old );
-			t = haxe.Timer.stamp() - t;
 
 			if( !dn.debug.MemTrack.allocs.exists($v{id}) )
-				dn.debug.MemTrack.allocs.set($v{id}, { total:0, time:0, calls:0 });
+				dn.debug.MemTrack.allocs.set($v{id}, { total:0, calls:0 });
 			var alloc = dn.debug.MemTrack.allocs.get( $v{id} );
 			alloc.total += m;
-			alloc.time += t;
 			alloc.calls++;
 		}
 	}
@@ -44,22 +45,61 @@ class MemTrack {
 	/** Reset current allocs tracking **/
 	public static function reset() {
 		allocs = new Map();
+		firstMeasure = -1;
+	}
+
+
+
+	static inline function padRight(str:String, minLen:Int, padChar=" ") {
+		while( str.length<minLen )
+			str+=padChar;
+		return str;
 	}
 
 	/** Print report to standard output **/
-	public static function report() {
+	public static function report(?printer:String->Void) {
+		if( printer==null )
+			printer = (v)->trace(v);
+
 		var all = [];
 		for(a in allocs.keyValueIterator())
 			all.push({id: a.key, mem:a.value });
 		all.sort( (a,b) -> -Reflect.compare(a.mem.total, b.mem.total) );
 
-		trace("MemTrack report");
-		trace("---------------");
+		if( all.length==0 ) {
+			printer("MemTrack has nothing to report.");
+			return;
+		}
+
+		printer("MEMTRACK REPORT");
+		var t = haxe.Timer.stamp() - firstMeasure;
+		printer('Elapsed time: ${M.pretty(t,1)}s');
+		var table = [];
 		for(a in all)
-			trace(
-				"  "+a.id+" => "+dn.M.unit(a.mem.total)+", "
-				+dn.M.unit(a.mem.total/a.mem.calls)+"/call "
-				+"("+dn.M.pretty(a.mem.time,2)+"s)"
-			);
+			table.push([
+				a.id,
+				dn.M.unit(a.mem.total),
+				dn.M.unit(a.mem.total/t)+"/s",
+				// dn.M.unit(a.mem.total/a.mem.calls)+"/call",
+				// a.mem.calls+" calls",
+			]);
+
+		// Build visual table
+		var colWidths : Array<Int> = [];
+		for(line in table) {
+			for(i in 0...line.length)
+				if( !M.isValidNumber(colWidths[i]) )
+					colWidths[i] = line[i].length;
+				else
+					colWidths[i] = M.imax(colWidths[i], line[i].length);
+		}
+		printer(colWidths.join(","));
+		for(line in table) {
+			for(i in 0...line.length)
+				line[i] = padRight(line[i], colWidths[i]);
+			printer("| " + line.join("  |  ") + " |");
+		}
+
+		reset();
 	}
 }
