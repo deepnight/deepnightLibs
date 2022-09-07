6c42425911e5934adc1e0b6b57ef696b6b5c2ec4
Sebastien Benard <github@deepnight.net>
2022-09-07 12:34:49 +0200

    Added zoom blur

--- src/dn/heaps/filter/MotionBlur.hx
+++ src/dn/heaps/filter/MotionBlur.hx
@@ -7,13 +7,15 @@ class MotionBlur extends h2d.filter.Shader<InternalShader> {
 
 	public var blurX(default,set): Float;
 	public var blurY(default,set): Float;
+	public var isZoomBlur(default,set): Bool;
 	public var ramps(default,set): Int;
 
 	/** Add a pixel-perfect outline around a h2d.Object using a shader filter **/
-	public function new(xBlurPx:Float, yBlurPx:Float, ramps=3) {
+	public function new(xBlurPx:Float, yBlurPx:Float, ramps=4) {
 		super( new InternalShader() );
 		this.blurX = xBlurPx;
 		this.blurY = yBlurPx;
+		this.isZoomBlur = false;
 		this.ramps = ramps;
 	}
 
@@ -28,6 +30,11 @@ class MotionBlur extends h2d.filter.Shader<InternalShader> {
 
 	inline function set_blurY(v:Float) {
 		shader.blurY = v;
+		return v;
+	}
+
+	inline function set_isZoomBlur(v:Bool) {
+		shader.isZoomBlur = v ? 1 : 0;
 		return v;
 	}
 
@@ -50,6 +57,7 @@ private class InternalShader extends h3d.shader.ScreenShader {
 		@param var texelSize : Vec2;
 		@param var blurX : Float;
 		@param var blurY : Float;
+		@param var isZoomBlur : Int;
 		@param var ramps : Float;
 
 		inline function getLum(col:Vec3) : Float {
@@ -62,13 +70,33 @@ private class InternalShader extends h3d.shader.ScreenShader {
 			for(i in 0...int(ramps)) {
 				var iLocal = i; // force local copy
 				var r : Float = (iLocal+1) / ramps;
-				var neighColor = texture.get( input.uv + vec2(texelSize.x*-blurX*(r*r-0.5), texelSize.y*-blurY*(r*r-0.5) ) );
-				curColor = mix( curColor, neighColor, 0.1+0.9*(1-r) );
+
+				if( isZoomBlur==1 ) {
+					// Zoom blur
+					var ang = atan(input.uv.y-0.5, input.uv.x-0.5);
+					var dist = distance( input.uv, vec2(0.5) );
+					var neighColor = texture.get(
+						input.uv +
+						vec2(
+							texelSize.x * -blurX * dist/0.6 * (0.2+0.8*r) * cos(ang),
+							texelSize.x * -blurY * dist/0.6 * (0.2+0.8*r) * sin(ang)
+						)
+					);
+					curColor = mix( curColor, neighColor, 0.1+0.9*(1-r) );
+				}
+				else {
+					// X/Y blurs
+					var neighColor = texture.get( input.uv + vec2(texelSize.x*-blurX*(r*r-0.5), texelSize.y*-blurY*(r*r-0.5) ) );
+					curColor = mix( curColor, neighColor, 0.1+0.9*(1-r) );
+				}
 			}
 
-			var original : Vec4 = texture.get(input.uv);
-			var lum = getLum(original.rgb);
-			curColor = mix( curColor, original, 0.8*lum );
+			// Increase importance of original pixels
+			if( isZoomBlur==0 ) {
+				var original : Vec4 = texture.get(input.uv);
+				var lum = getLum(original.rgb);
+				curColor = mix( curColor, original, 0.8*lum );
+			}
 
 			output.color = curColor;
 		}
