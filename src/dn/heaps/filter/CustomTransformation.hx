fe9a04251114b951691bd67fbc963b01b9202de1
Sebastien Benard <github@deepnight.net>
2022-09-08 16:47:01 +0200

    Added experimental rectangular zoom

--- src/dn/heaps/filter/CustomTransformation.hx
+++ src/dn/heaps/filter/CustomTransformation.hx
@@ -6,10 +6,13 @@ class CustomTransformation extends h2d.filter.Shader<InternalShader> {
 	public var scaleY(default,set): Float;
 	public var scalePivotX(default,set): Float;
 	public var scalePivotY(default,set): Float;
+
 	public var skewScaleLeft(default,set) : Float;
 	public var skewScaleRight(default,set) : Float;
 	public var skewScaleTop(default,set) : Float;
 	public var skewScaleBottom(default,set) : Float;
+
+	public var rectangularZoom(default,set) : Float;
 
 
 	public function new() {
@@ -21,6 +24,7 @@ class CustomTransformation extends h2d.filter.Shader<InternalShader> {
 	public function reset() {
 		setScale(1);
 		setScalePivots(0,0);
+		rectangularZoom = 0;
 		resetSkews();
 	}
 
@@ -40,14 +44,17 @@ class CustomTransformation extends h2d.filter.Shader<InternalShader> {
 		scalePivotY = py;
 	}
 
-	inline function set_scaleX(v:Float) { shader.scaleX = v; return v; }
-	inline function set_scaleY(v:Float) { shader.scaleY = v; return v; }
-	inline function set_scalePivotX(v:Float) { shader.scalePivotX = v; return v; }
-	inline function set_scalePivotY(v:Float) { shader.scalePivotY = v; return v; }
-	inline function set_skewScaleLeft(v:Float) { shader.skewScaleLeft = v; return v; }
-	inline function set_skewScaleRight(v:Float) { shader.skewScaleRight = v; return v; }
-	inline function set_skewScaleTop(v:Float) { shader.skewScaleTop = v; return v; }
-	inline function set_skewScaleBottom(v:Float) { shader.skewScaleBottom = v; return v; }
+	inline function set_scaleX(v:Float) return shader.scaleX = v;
+	inline function set_scaleY(v:Float) return shader.scaleY = v;
+	inline function set_scalePivotX(v:Float) return shader.scalePivotX = v;
+	inline function set_scalePivotY(v:Float) return shader.scalePivotY = v;
+
+	inline function set_skewScaleLeft(v:Float) return shader.skewScaleLeft = v;
+	inline function set_skewScaleRight(v:Float) return shader.skewScaleRight = v;
+	inline function set_skewScaleTop(v:Float) return shader.skewScaleTop = v;
+	inline function set_skewScaleBottom(v:Float) return shader.skewScaleBottom = v;
+
+	inline function set_rectangularZoom(v:Float) return shader.rectangularZoom = v;
 
 	override function sync(ctx : h2d.RenderContext, s : h2d.Object) {
 		super.sync(ctx, s);
@@ -66,20 +73,17 @@ private class InternalShader extends h3d.shader.ScreenShader {
 		@param var texture : Sampler2D;
 		@param var texelSize : Vec2;
 
-		@param var blurX : Float;
-		@param var blurY : Float;
-		@param var isZoomBlur : Int;
-		@param var zoomBlurOriginU : Float;
-		@param var zoomBlurOriginV : Float;
-
 		@param var scaleX : Float;
 		@param var scaleY : Float;
 		@param var scalePivotX : Float;
 		@param var scalePivotY : Float;
+
 		@param var skewScaleLeft : Float;
 		@param var skewScaleRight : Float;
 		@param var skewScaleTop : Float;
 		@param var skewScaleBottom : Float;
+
+		@param var rectangularZoom : Float;
 
 		@param var ramps : Float;
 
@@ -109,7 +113,19 @@ private class InternalShader extends h3d.shader.ScreenShader {
 			skewScale = skewScaleTop + uv.y * ( skewScaleBottom - skewScaleTop );
 			uv.x = uv.x / skewScale  +  0.5  -  0.5/skewScale;
 
-			output.color = getSrcColor(uv);
+			// Zoom scale
+			if( rectangularZoom!=0 ) {
+				var centerU = 0.5;
+				var centerV = 0.45;
+				var distX = ( uv.x - centerU ) * 0.7;
+				var distY = ( uv.y - centerV ) * 0.7;
+				uv.x = uv.x + abs(distX) * distX * -rectangularZoom;
+				uv.y = uv.y + abs(distY) * distY * -rectangularZoom;
+				output.color = texture.get(uv);
+			}
+			else
+				output.color = getSrcColor(uv);
+
 		}
 	};
 }
