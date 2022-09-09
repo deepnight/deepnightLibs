b5836b0f30143e1e49c916680932b0f611b0f0ed
Sebastien Benard <github@deepnight.net>
2022-09-09 15:14:36 +0200

    Removed Scaler.bestFit_f argument

--- src/dn/heaps/Scaler.hx
+++ src/dn/heaps/Scaler.hx
@@ -9,20 +9,16 @@ class Scaler {
 
 
 	/** Fit `wid`x`hei` in current viewport, optionally snapping to closest Integer scale value (for pixel perfect rendering) **/
-	public static function bestFit_f(widPx:Float, ?heiPx:Float, ?contextWid:Float, ?contextHei:Float, integerScale=true) : Float {
+	public static function bestFit_f(widPx:Float, ?heiPx:Float, ?contextWid:Float, ?contextHei:Float) : Float {
 		var sx = ( contextWid==null ? getViewportWidth() : contextWid ) / widPx;
 		var sy = ( contextHei==null ? getViewportHeight() : contextHei ) / ( heiPx==null ? widPx : heiPx );
-		if( integerScale ) {
-			sx = M.floor(sx);
-			sy = M.floor(sy);
-		}
 		return M.fmax(1, M.fmin(sx,sy) );
 	}
 
 
 	/** Fit `wid`x`hei` in current viewport, while keeping scaling value as Int **/
 	public static inline function bestFit_i(widPx:Float, ?heiPx:Float, ?contextWid:Float, ?contextHei:Float) : Int {
-		return Std.int( bestFit_f(widPx, heiPx, contextWid, contextHei, true) );
+		return M.floor( bestFit_f(widPx, heiPx, contextWid, contextHei) );
 	}
 
 
