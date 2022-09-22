3a4bdafa3013913893b0b9f5cf4b15488370bda1
Sebastien Benard <github@deepnight.net>
2022-09-22 09:48:17 +0200

    Added Invert filter

---
+++ src/dn/heaps/filter/Invert.hx
@@ -0,0 +1,24 @@
+package dn.heaps.filter;
+
+
+class Invert extends h2d.filter.Shader<InternalShader> {
+	public function new() {
+		super( new InternalShader() );
+	}
+}
+
+
+// --- Shader -------------------------------------------------------------------------------
+private class InternalShader extends h3d.shader.ScreenShader {
+	static var SRC = {
+		@param var texture : Sampler2D;
+
+		function fragment() {
+			var col = texture.get(input.uv);
+			col.r = 1-col.r;
+			col.g = 1-col.g;
+			col.b = 1-col.b;
+			output.color = col;
+		}
+	};
+}
