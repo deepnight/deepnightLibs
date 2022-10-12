dc93f3056f20b35707f8f3e60dc1f75812e81963
Sebastien Benard <github@deepnight.net>
2022-10-12 10:37:07 +0200

    Added quick access to base colors

--- src/dn/heaps/Palette.hx
+++ src/dn/heaps/Palette.hx
@@ -3,12 +3,50 @@ package dn.heaps;
 class Palette {
 	var colors : Array<Col> = [];
 
-	public var red(default,null) : Col;
-	public var green(default,null) : Col;
-	public var blue(default,null) : Col;
+	public var black: Col;
+	public var white: Col;
+	public var yellow: Col;
+
+	public var lightRed: Col;
+	public var lightGreen: Col;
+	public var lightBlue: Col;
+	public var lightOrange: Col;
+	public var lightCyan: Col;
+	public var lightPink: Col;
+	public var lightGray: Col;
+
+	public var darkRed: Col;
+	public var darkGreen: Col;
+	public var darkBlue: Col;
+	public var darkOrange: Col;
+	public var darkCyan: Col;
+	public var darkPink: Col;
+	public var darkGray: Col;
 
 	public function new() {
 		refreshQuickAccesses();
+	}
+
+	function refreshQuickAccesses() {
+		black = getClosest("#000");
+		white = getClosest("#fff");
+		yellow = getClosest("#ff0");
+
+		lightRed = getClosest("#f00");
+		lightGreen = getClosest("#0f0");
+		lightBlue = getClosest("#00f");
+		lightOrange = getClosest("#fa0");
+		lightCyan = getClosest("#0ff");
+		lightPink = getClosest("f0f");
+		lightGray = getClosest("bbb");
+
+		darkRed = getClosest("#800");
+		darkGreen = getClosest("#080");
+		darkBlue = getClosest("#008");
+		darkOrange = getClosest("#840");
+		darkCyan = getClosest("#088");
+		darkPink = getClosest("#808");
+		darkGray = getClosest("#888");
 	}
 
 	public function addColor(c:Col) {
@@ -30,30 +68,23 @@ class Palette {
 			x+=colorSize;
 		}
 		p.refreshQuickAccesses();
-		trace(p);
 
 		return p;
 	}
 
-	inline function getClosest(target:Col) {
+	public inline function getClosest(target:Col) {
 		var closest : Col = 0x0;
 		var closestDist = 99.;
+		var dist = 0.;
 		for(c in colors) {
-			var dist = target.getDistance(c);
+			dist = target.getDistance(c);
 			if( dist<closestDist ) {
 				closestDist = dist;
 				closest = c;
+				if( dist==0 )
+					break;
 			}
 		}
 		return closest;
-	}
-
-	function refreshQuickAccesses() {
-		red = getClosest("#f00");
-		green = getClosest("#0f0");
-		blue = getClosest("#00f");
-		trace(red);
-		trace(green);
-		trace(blue);
 	}
 }
