b769f1add13969e8ad342a446a5590c05916da0f
Sebastien Benard <github@deepnight.net>
2022-11-07 10:10:36 +0100

    Added width/height and pivots to TiledTexture

--- src/dn/heaps/TiledTexture.hx
+++ src/dn/heaps/TiledTexture.hx
@@ -1,23 +1,50 @@
 package dn.heaps;
 
 class TiledTexture extends h2d.TileGroup {
+	var invalidated = true;
+	public var width(default,set) : Int;
+	public var height(default,set) : Int;
+
+	public var pivotX(default,set) = 0.;
+	public var pivotY(default,set) = 0.;
+
 	public function new(texTile:h2d.Tile, wid:Int, hei:Int, ?p:h2d.Object) {
 		super(texTile, p);
-		resize(wid,hei);
+		width = wid;
+		height = hei;
 	}
 
-	public function resize(wid:Int,hei:Int) {
+	inline function set_width(v) {  invalidated = true; return width = v;  }
+	inline function set_height(v) {  invalidated = true; return height = v;  }
+	inline function set_pivotX(v) {  invalidated = true; return pivotX = v;  }
+	inline function set_pivotY(v) {  invalidated = true; return pivotY = v;  }
+
+	public inline function resize(wid:Int,hei:Int) {
+		width = wid;
+		height = hei;
+	}
+
+	function build() {
 		clear();
 		var x = 0;
 		var y = 0;
-		while( y<hei) {
-			add( x, y, tile.sub( 0, 0, M.fmin(wid-x,tile.width), M.fmin(hei-y,tile.height) ) );
+		var ox = M.round( -pivotX*width );
+		var oy = M.round( -pivotY*height );
+		while( y<height) {
+			add( x+ox, y+oy, tile.sub( 0, 0, M.fmin(width-x,tile.width), M.fmin(height-y,tile.height) ) );
 			x += Std.int(tile.width);
-			if( x>=wid ) {
+			if( x>=width ) {
 				x = 0;
 				y += Std.int(tile.height);
 			}
 		}
+	}
 
+	override function sync(ctx:h2d.RenderContext) {
+		if( invalidated ) {
+			invalidated = false;
+			build();
+		}
+		super.sync(ctx);
 	}
 }
