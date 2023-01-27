01b88216cd66e180e7f511c34d88fdd80c14ad30
Sebastien Benard <github@deepnight.net>
2023-01-27 09:51:12 +0100

    Sorted tests classes in alpha order

--- tests/Tests.hx
+++ tests/Tests.hx
@@ -14,34 +14,32 @@ class Tests {
 		CiAssert.isNotNull("a");
 
 		// Libs
-		dn.Lib.__test();
+		dn.Args.__test();
+		dn.pathfinder.AStar.__test();
+		dn.Bresenham.__test();
+		dn.Changelog.__test();
 		dn.Cinematic.__test();
+		dn.Col.UnitTest._test();
+		dn.legacy.Color.__test();
 		dn.Cooldown.__test();
 		dn.DecisionHelper.__test();
 		dn.Delayer.__test();
-		dn.Rand.__test();
-		dn.Col.UnitTest._test();
-		dn.Bresenham.__test();
 		dn.FilePath.__test();
-		dn.Version.__test();
-		dn.M.__test();
-		dn.Changelog.__test();
-		dn.Args.__test();
-		dn.Process.__test();
-		dn.RandomTools.__test();
-
-		dn.pathfinder.AStar.__test();
-
-		dn.legacy.Color.__test();
-		dn.legacy.GetText.__test();
-
-		dn.data.GetText.__test();
-		dn.data.LocalStorage.__test();
-
-		dn.struct.RandList.__test();
-		dn.struct.RandDeck.__test();
 		dn.struct.FixedArray.__test();
+		dn.data.GetText.__test();
+		dn.legacy.GetText.__test();
+		dn.Lib.__test();
+		dn.data.LocalStorage.__test();
+		dn.M.__test();
+		dn.Process.__test();
+		dn.Rand.__test();
+		dn.struct.RandDeck.__test();
+		dn.struct.RandList.__test();
+		dn.RandomTools.__test();
 		dn.struct.RecyclablePool.__test();
+		dn.Version.__test();
+
+
 
 		// Done!
 		Lib.println("");
