ec71f0a581ae668dab6b9c4b4309eec7ce238ee0
Sebastien Benard <github@deepnight.net>
2023-10-27 10:18:40 +0200

    Added points checks

--- src/dn/Geom.hx
+++ src/dn/Geom.hx
@@ -2,25 +2,7 @@ package dn;
 
 class Geom {
 
-	/** Return TRUE if rectangle A touches or Collides rectangle B. **/
-	// public static inline function rectTouchesRect(aX:Float, aY:Float, aWid:Float, aHei:Float, bX:Float, bY:Float, bWid:Float, bHei:Float) {
-	// 	/*
-	// 	Source: https://www.baeldung.com/java-check-if-two-rectangles-overlap
-
-	// 	"The two given rectangles won't overlap if either of the below conditions is true:
-	// 		One of the two rectangles is above the top edge of the other rectangle
-	// 		One of the two rectangles is on the left side of the left edge of the other rectangle"
-	// 	*/
-	// 	if( aY+aHei < bY || bY+bHei < aY )
-	// 		return false;
-	// 	else if( aX+aWid < bX || bX+bWid < aX )
-	// 		return false;
-	// 	else
-	// 		return true;
-	// }
-
-
-	/** Return TRUE if rectangle A Collides rectangle B. **/
+	/** Check if rectangle A collides rectangle B. **/
 	public static inline function rectCollidesRect(aX:Float, aY:Float, aWid:Float, aHei:Float, bX:Float, bY:Float, bWid:Float, bHei:Float) {
 		/*
 		Source: https://www.baeldung.com/java-check-if-two-rectangles-overlap
@@ -37,6 +19,7 @@ class Geom {
 			return true;
 	}
 
+	/** Check if a rectangle collides with a circle **/
 	public static inline function rectCollidesCircle(rx:Float, ry:Float, rWid:Float, rHei:Float, circleX:Float, circleY:Float, radius:Float) {
 		// Find the closest point on the rectangle to the center of the circle
 		final closestX = M.fclamp(circleX, rx, rx+rWid);
@@ -51,9 +34,21 @@ class Geom {
 	}
 
 
-
+	/** Check if a rectangle circle A with a circle B **/
 	public static inline function circleCollidesCircle(xA:Float, yA:Float, rA:Float,  xB:Float, yB:Float, rB:Float) {
 		return M.distSqr(xA,yA,xB,yB) <= (rA+rB)*(rA+rB);
+	}
+
+
+	/** Check if a coordinate is inside a rectangle **/
+	public static inline function pointInsideRect(ptX:Float, ptY:Float, rx:Float, ry:Float, rWid:Float, rHei:Float) {
+		return ptX>=rx && ptX<rx+rWid && ptY>=ry && ptY<ry+rHei;
+	}
+
+
+	/** Check if a coordinate is inside a circle **/
+	public static inline function pointInsideCircle(ptX:Float, ptY:Float, circleX:Float, circleY:Float, radius:Float) {
+		return M.distSqr(ptX,ptY, circleX,circleY) <= radius*radius;
 	}
 
 
