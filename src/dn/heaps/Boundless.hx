a369a62fbccf11658dee7becf3d58a10e22196d9
Sebastien Benard <github@deepnight.net>
2024-02-01 21:31:11 +0100

    Added Boundless object

---
+++ src/dn/heaps/Boundless.hx
@@ -0,0 +1,12 @@
+package dn.heaps;
+
+/**
+	The Bounds of this utility class are always empty, even if it has children.
+	This is useful with h2d.Flow, to add a child that doesn't affect the Flow bounds.
+**/
+class Boundless extends h2d.Object {
+	override function getBoundsRec(relativeTo:h2d.Object, out:h2d.col.Bounds, forSize:Bool) {
+		super.getBoundsRec(relativeTo, out, forSize);
+		out.empty();
+	}
+}
