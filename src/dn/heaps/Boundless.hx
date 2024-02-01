ba3d368f0e9fe0f3d3e0aa9ee8f996746ea6f9a9
Sebastien Benard <github@deepnight.net>
2024-02-01 22:37:45 +0100

    Better Boundless constructor

--- src/dn/heaps/Boundless.hx
+++ src/dn/heaps/Boundless.hx
@@ -5,6 +5,12 @@ package dn.heaps;
 	This is useful with h2d.Flow, to add a child that doesn't affect the Flow bounds.
 **/
 class Boundless extends h2d.Object {
+	public function new(setAbsoluteInParentFlow=false, ?parent) {
+		super(parent);
+		if( setAbsoluteInParentFlow && Std.isOfType(parent,h2d.Flow) )
+			Std.downcast(parent, h2d.Flow).getProperties(this).isAbsolute = true;
+	}
+
 	override function getBoundsRec(relativeTo:h2d.Object, out:h2d.col.Bounds, forSize:Bool) {
 		super.getBoundsRec(relativeTo, out, forSize);
 		out.empty();
