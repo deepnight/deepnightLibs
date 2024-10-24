53f80ad64e88e7e1c4b31ba8341111c2f64c4700
Sebastien Benard <github@deepnight.net>
2024-10-24 16:46:19 +0200

    UPD cleanup HSpriteBatch

--- src/dn/heaps/slib/HSpriteBatch.hx
+++ src/dn/heaps/slib/HSpriteBatch.hx
@@ -6,35 +6,35 @@ class HSpriteBatch extends h2d.SpriteBatch {
 
 	override function onAdd(){
 		super.onAdd();
-		var c = first;
-		while( c != null ){
-			if( Std.isOfType(c,HSpriteBE) )
-				(cast c:HSpriteBE).onAdd();
-			c = c.next;
+		var e = first;
+		while( e != null ){
+			if( Std.isOfType(e,HSpriteBE) )
+				Std.downcast(e, HSpriteBE).onAdd();
+			e = e.next;
 		}
 	}
 
 	override function onRemove(){
 		super.onRemove();
-		var c = first;
-		while( c != null ){
-			if( Std.isOfType(c,HSpriteBE) )
-				(cast c:HSpriteBE).onRemove();
-			c = c.next;
+		var e = first;
+		while( e != null ){
+			if( Std.isOfType(e,HSpriteBE) )
+				Std.downcast(e, HSpriteBE).onRemove();
+			e = e.next;
 		}
 	}
 
 	override function add( e : BatchElement, before=false ){
 		e = super.add(e,before);
 		if( allocated && Std.isOfType(e,HSpriteBE) )
-			(cast e:HSpriteBE).onAdd();
+			Std.downcast(e, HSpriteBE).onAdd();
 		return e;
 	}
 
 	override function delete( e : BatchElement ){
 		super.delete(e);
 		if( allocated && Std.isOfType(e,HSpriteBE) )
-			(cast e:HSpriteBE).onRemove();
+			Std.downcast(e, HSpriteBE).onRemove();
 	}
 
 }
