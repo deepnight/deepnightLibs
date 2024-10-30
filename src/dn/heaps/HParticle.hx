26809767ad0e65cf423394806feaa59c0977d79f
Sebastien Benard <github@deepnight.net>
2024-10-30 18:16:12 +0100

    UPD default squash durations

--- src/dn/heaps/HParticle.hx
+++ src/dn/heaps/HParticle.hx
@@ -165,12 +165,12 @@ class HParticle extends BatchElement {
 		scaleY_tween.start(from, to, durationS);
 	}
 
-	public inline function squashX(s:Float, durationS=0.1) {
+	public inline function squashX(s:Float, durationS=0.06) {
 		scaleX_tween.start(scaleX*s, scaleX, durationS);
 		scaleY_tween.start(scaleX*(2-s), scaleY, durationS);
 	}
 
-	public inline function squashY(s:Float, durationS=0.1) {
+	public inline function squashY(s:Float, durationS=0.06) {
 		scaleX_tween.start(scaleX*(2-s), scaleX, durationS);
 		scaleY_tween.start(scaleY*s, scaleY, durationS);
 	}
