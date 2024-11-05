b4bde056b8d9a1349589be8fcce73e4b114b5cc8
Sebastien Benard <github@deepnight.net>
2024-11-05 17:25:55 +0100

    UPD reworked FlowBg to support removeChildren()

--- src/dn/heaps/FlowBg.hx
+++ src/dn/heaps/FlowBg.hx
@@ -1,33 +1,53 @@
 package dn.heaps;
 
 class FlowBg extends h2d.Flow {
-	public var bg : h2d.ScaleGrid;
+	var _bg : Null<h2d.ScaleGrid>;
+
+	var bgTile : Null<h2d.Tile>;
+	var bgBorderHorizontal : Int;
+	var bgBorderVertical : Int;
+	var bgColor : Col;
+
 
 	public function new(?bgTile:h2d.Tile, horizontalBorder=1, ?verticalBorder:Int, ?p:h2d.Object) {
 		super(p);
 
-		bg = new h2d.ScaleGrid(bgTile, 1,1, this);
-		getProperties(bg).isAbsolute = true;
-		setBgBorder(horizontalBorder, verticalBorder);
+		this.bgTile = bgTile;
+		this.bgBorderHorizontal = horizontalBorder;
+		this.bgBorderVertical = verticalBorder ?? horizontalBorder;
 	}
 
-	public function setBg(t:h2d.Tile, horizontalBorder:Int, ?verticalBorder:Int) {
-		bg.tile = t;
-		setBgBorder(horizontalBorder, verticalBorder);
+	public inline function setBg(t:h2d.Tile, horizontalBorder:Int, ?verticalBorder:Int) {
+		bgTile = t;
+		bgBorderHorizontal = horizontalBorder;
+		bgBorderVertical = verticalBorder ?? horizontalBorder;
 	}
 
 	public inline function setBgBorder(horizontal:Int, ?vertical:Int) {
-		bg.borderLeft = bg.borderRight = horizontal;
-		bg.borderTop = bg.borderBottom = vertical!=null ? vertical : horizontal;
+		bgBorderHorizontal = horizontal;
+		bgBorderVertical = vertical!=null ? vertical : horizontal;
 	}
 
-	public inline function colorizeBg(c:UInt) {
-		bg.color.setColor( dn.legacy.Color.addAlphaF(c) );
+	public inline function colorizeBg(c:Col, alpha=1.0) {
+		bgColor = c.withAlpha(alpha);
 	}
 
 	override function sync(ctx:h2d.RenderContext) {
 		super.sync(ctx);
-		bg.width = outerWidth;
-		bg.height = outerHeight;
+
+		if( bgTile!=null ) {
+			if( _bg==null || _bg.parent==null ) {
+				_bg = new h2d.ScaleGrid(bgTile, 1,1);
+				addChildAt(_bg, 0);
+			}
+
+			_bg.tile = bgTile;
+			getProperties(_bg).isAbsolute = true;
+			_bg.borderLeft = _bg.borderRight = bgBorderHorizontal;
+			_bg.borderTop = _bg.borderBottom = bgBorderVertical;
+			_bg.color.setColor( bgColor );
+			_bg.width = outerWidth;
+			_bg.height = outerHeight;
+		}
 	}
 }
