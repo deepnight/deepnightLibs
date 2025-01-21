85e1708d2c00632de369569c7787a468bf3222c1
Sebastien Benard <github@deepnight.net>
2025-01-21 15:33:38 +0100

    UPD better valid state checks

--- src/dn/TinyTween.hx
+++ src/dn/TinyTween.hx
@@ -14,7 +14,7 @@ class TinyTween {
 	var toValue : Float;
 	var elapsedS : Float;
 	var durationS : Float;
-	var interp : TinyTweenInterpolation = Linear;
+	var interp : TinyTweenInterpolation = EaseInOut;
 	public var curValue(get,never) : Float;
 
 	public inline function new(fps) {
@@ -22,12 +22,21 @@ class TinyTween {
 		reset();
 	}
 
+	/** Reset the tween completely **/
 	public inline function reset() {
 		fromValue = toValue = 0;
-		durationS = elapsedS = 0;
+		durationS = elapsedS = -1;
+	}
+
+	/** Return TRUE if the tween has any valid value (ie. it's either started, or completed). Reseting makes this function FALSE. **/
+	public inline function hasAnyValue() {
+		return durationS>0;
 	}
 
 	inline function get_curValue() {
+		if( !hasAnyValue() )
+			return 0;
+
 		if( isComplete() )
 			return switch interp {
 				case BackForth: fromValue;
@@ -45,14 +54,14 @@ class TinyTween {
 	}
 
 	public inline function isComplete() {
-		return elapsedS>=durationS;
+		return hasAnyValue() && elapsedS>=durationS;
 	}
 
 	public inline function getElapsedRatio() : Float {
-		return isComplete() ? 1 : elapsedS/durationS;
+		return !hasAnyValue() ? 0 : isComplete() ? 1 : elapsedS/durationS;
 	}
 
-	public inline function start(from:Float, to:Float, durationS:Float, interp:TinyTweenInterpolation=EaseInOut) {
+	public inline function start(from:Float, to:Float, durationS:Float, interp:TinyTweenInterpolation = EaseInOut) {
 		this.fromValue = from;
 		this.toValue = to;
 		this.durationS = durationS;
@@ -60,9 +69,14 @@ class TinyTween {
 		this.interp = interp;
 	}
 
+	public function restartFrom(from:Float) {
+		elapsedS = 0;
+		fromValue = from;
+	}
+
 	/** Advance the tween, return TRUE if complete **/
 	public inline function update(tmod:Float) : Bool {
-		if( isComplete() )
+		if( !hasAnyValue() || isComplete() )
 			return false;
 
 		elapsedS = M.fmin( durationS, elapsedS + tmod/fps );
@@ -75,9 +89,11 @@ class TinyTween {
 		var v = 0.;
 		var fps = 10;
 		var t = new TinyTween(fps);
+		CiAssert.equals( t.hasAnyValue(), false );
 
 		// Linear
 		t.start(10, 20, 1, Linear);
+		CiAssert.equals( t.hasAnyValue(), true );
 		CiAssert.equals( t.curValue, 10 );
 		CiAssert.equals( t.isComplete(), false );
 		t.update(fps*0.5);
@@ -86,6 +102,7 @@ class TinyTween {
 		t.update(fps*0.5);
 		CiAssert.equals( M.round(t.curValue), 20 );
 		CiAssert.equals( t.isComplete(), true );
+		CiAssert.equals( t.hasAnyValue(), true );
 
 		t.start(10, 20, 1, EaseIn);
 		CiAssert.equals( t.curValue, 10 );
