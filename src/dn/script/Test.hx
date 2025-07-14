5605046a605692be23bdf16828d809915d8271a4
Sebastien Benard <github@deepnight.net>
2025-07-14 16:00:02 +0200

    UPD api tests

--- src/dn/script/Test.hx
+++ src/dn/script/Test.hx
@@ -29,17 +29,17 @@ class Test {
 		CiAssert.throwsException( "Unknown identifier (api call)", r.run("api.unknown();") );
 		CiAssert.throwsException( "Invalid characters", r.run("var x=1; ù$^!ù;") );
 		CiAssert.throwsException( "Incompatible types", r.run("var x=5; x='';") );
-		CiAssert.throwsException( "Incompatible arg types", r.run("var x=pow('a');") );
+		CiAssert.throwsException( "Incompatible arg types", r.run("var x=pow2('a');") );
 		CiAssert.throwsException( "Undefined var", r.run("x=1") );
 
 		r.throwErrors = false;
 
-		// Function calls
-		CiAssert.equals( { Api.retValue=-1; r.run("reset();"); Api.retValue; },  0 );
-		CiAssert.equals( { r.run("api.ret(1);"); Api.retValue; },  1 );
-		CiAssert.equals( { r.run("ret(1);"); Api.retValue; },  1 );
-		CiAssert.equals( { r.run("ret( pow(2) );"); Api.retValue; },  4 );
-		CiAssert.equals( { r.run("ret( api.pow(2) );"); Api.retValue; },  4 );
+		// API calls
+		CiAssert.equals( { r.run("var x=pow2(5); x;"); r.output_int; },  25 );
+		CiAssert.equals( { r.run("var x=pow2(1.5); x;"); r.output_int; },  2 );
+		CiAssert.equals( { r.run("var x=pow2(1.5); x;"); r.output_float; },  2.25 );
+		CiAssert.equals( { r.run("var x=inc(1); x;"); r.output_int; },  2 );
+		CiAssert.equals( { r.run("var x=1; inc(x); x;"); r.output_int; },  1 );
 
 
 		// Output: Dynamic
@@ -48,7 +48,8 @@ class Test {
 		CiAssert.equals( { r.run("5"); r.output; },  5 );
 		CiAssert.equals( { r.run("5.3"); r.output; },  5.3 );
 		CiAssert.equals( { r.run("true"); r.output; },  true );
-		CiAssert.equals( { r.run("y=5; y;"); r.output; },  null );
+		CiAssert.equals( { r.run("var x=5; x=''; x;"); r.output; },  null );
+		CiAssert.equals( { r.run("unknown_var;"); r.output; },  null );
 
 		// Output: Int
 		CiAssert.equals( { r.run(""); r.output_int; },  0 );
@@ -88,15 +89,8 @@ class Test {
 
 @:rtti @:keep
 private class Api {
-	public static var retValue : Int;
-	// JS BUG: for some reason, the script cant't acceess "this.retValue" when using the script "ret(1);".
-	// It seems script scope isn't correct in JS.
-	// For the tests to work, retValue must be static (no "this" access)
+	public function new() {}
 
-	public function new() {
-		reset();
-	}
-	public function reset() retValue = 0;
-	public function pow(v:Int) return v*v;
-	public function ret(v:Int) retValue = v;
+	public function pow2(v:Float) return v*v;
+	public function inc(v:Float) return v+1;
 }
