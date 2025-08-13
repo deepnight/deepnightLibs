57c810d9476488f0bd96d7501a3549a957fd3348
Sebastien Benard <github@deepnight.net>
2025-08-13 11:47:10 +0200

    UPD (breaking) reworked Velocity to support preMultiplication of values

--- src/dn/phys/Velocity.hx
+++ src/dn/phys/Velocity.hx
@@ -3,62 +3,83 @@ package dn.phys;
 /**
 	A generic X/Y velocity utility class
 **/
+@:allow(dn.phys.VelocityArray)
 class Velocity {
 	/** Custom identifier **/
 	public var id = -1;
 
-	public var x : Float;
-	public var y : Float;
+	public var x(get,never) : Float;
+	public var y(get,never) : Float;
 	public var frictX : Float;
 	public var frictY : Float;
 
-	/** `v` is a convenience alias for `x` when you just need a 1D velocity **/
-	public var v(get,set) : Float;
-		inline function get_v() return x;
-		inline function set_v(v:Float) return setBoth(v);
+	/** Real internal X value, with preMul applied **/
+	var _preMultX : Float;
 
-	/** `x` alias **/
-	public var dx(get,set) : Float;
-		inline function set_dx(v) return x = v;
-		inline function get_dx() return x;
+	/** Real internal Y value, with preMul applied **/
+	var _preMultY : Float;
 
-	/** `y` alias **/
-	public var dy(get,set) : Float;
-		inline function set_dy(v) return y = v;
-		inline function get_dy() return y;
+	/** This factor is pre-multiplied to any value added to the X/Y velocity values **/
+	public var preMul : Float = 1.;
 
-	/** If absolute `x` or `y` goes below this value, instead, it is set to zero during next update. **/
+	/** Convenience alias for `x` **/
+	public var dx(get,never) : Float;
+		inline function get_dx() return _preMultX;
+
+	/** Convenience alias for `y` **/
+	public var dy(get,never) : Float;
+		inline function get_dy() return _preMultY;
+
+	/** If any absolute values of `x` or `y` goes below this value, instead, it is set to zero during next update. **/
 	public var clearThreshold = 0.0005;
 
 	public var frict(never,set) : Float;
 		inline function set_frict(v) return frictX = frictY = v;
 
 	/** Angle in radians of the vector represented by x/y **/
-	public var ang(get,never) : Float; inline function get_ang() return Math.atan2(y,x);
+	public var ang(get,never) : Float; inline function get_ang() return Math.atan2(_preMultY,_preMultX);
 
 	/** Length of the vector represented by x/y **/
-	public var len(get,never) : Float; inline function get_len() return Math.sqrt(x*x + y*y);
+	public var len(get,never) : Float; inline function get_len() return Math.sqrt(_preMultX*_preMultX + _preMultY*_preMultY);
 
 	/** Sign of X (-1 or 1) **/
-	public var dirX(get,never) : Int; inline function get_dirX() return M.sign(x);
+	public var dirX(get,never) : Int; inline function get_dirX() return M.sign(_preMultX);
 
 	/** Sign of Y (-1 or 1) **/
-	public var dirY(get,never) : Int; inline function get_dirY() return M.sign(y);
+	public var dirY(get,never) : Int; inline function get_dirY() return M.sign(_preMultY);
 
 
 	public inline function new() {
-		x = y = 0;
+		_preMultX = _preMultY = 0;
 		frict = 1;
 	}
 
+	inline function get_x() return _preMultX;
+	public inline function setX(v:Float) _preMultX = v * preMul;
+	public inline function addX(v:Float) _preMultX += v * preMul;
+	public inline function mulX(v:Float) _preMultX *= v;
+	public inline function setSignX(sign:Int) {
+		if( sign!=0 )
+			_preMultX = M.fabs(_preMultX) * (sign>0?1:-1);
+	}
+
+	inline function get_y() return _preMultY;
+	public inline function setY(v:Float) _preMultY = v * preMul;
+	public inline function addY(v:Float) _preMultY += v * preMul;
+	public inline function mulY(v:Float) _preMultY *= v;
+	public inline function setSignY(sign:Int) {
+		if( sign!=0 )
+			_preMultY = M.fabs(_preMultY) * (sign>0?1:-1);
+	}
+
 	/** Create a Velocity instance with X and Y values **/
-	public static inline function createXY(x:Float, y:Float, frict=1.) {
+	public static inline function createXY(vx:Float, vy:Float, frict=1.) {
 		var v = new Velocity();
-		v.set(x,y);
+		v.setXY(vx,vy);
 		v.frict = frict;
 		return v;
 	}
-	@:deprecated("Use createXY(f)") @:noCompletion public inline function createInit(x,y,?f) createXY(x,y,f);
+	@:deprecated("Use createXY(f)") @:noCompletion public inline function createInit(vx,vy,?f) createXY(vx,vy,f);
 
 	/** Create a Velocity instance with an angle and a length **/
 	public static inline function createAng(ang:Float, len:Float, frict=1.) {
@@ -80,7 +101,7 @@ class Velocity {
 	}
 
 	public inline function shortString() {
-		return '${ M.pretty(x,2) },${ M.pretty(y,2) }';
+		return '${ M.pretty(_preMultX,2) },${ M.pretty(_preMultY,2) }';
 	}
 
 	/** Set individual frictions **/
@@ -91,68 +112,69 @@ class Velocity {
 
 	/* Multiply X/Y values by individual factors */
 	public inline function mulXY(fx:Float, fy:Float) {
-		x*=fx;
-		y*=fy;
+		mulX(fx);
+		mulY(fy);
 	}
 
 	/** Multiply both X/Y values by a factor **/
 	public inline function mul(f:Float) {
-		x*=f;
-		y*=f;
+		mulX(f);
+		mulY(f);
 	}
 	@:deprecated("Use mul(f)") @:noCompletion public inline function mulBoth(f:Float) mul(f);
 
 	public inline function clear() {
-		x = y = 0;
+		_preMultX = _preMultY = 0;
 	}
 
 	/** Add individual values to X/Y **/
 	public inline function addXY(vx:Float, vy:Float) {
-		x += vx;
-		y += vy;
+		addX(vx);
+		addY(vy);
 	}
 
 	public inline function addLen(v:Float) {
 		var l = len;
 		var a = ang;
-		x = Math.cos(a)*(l+v);
-		y = Math.sin(a)*(l+v);
+		_preMultX = Math.cos(a)*(l + v*preMul);
+		_preMultY = Math.sin(a)*(l + v*preMul);
 	}
 	@:deprecated("This method is no longer implemented.") @:noCompletion public inline function addBoth(v:Float) {}
 
 	/** Set X and Y to specific values **/
-	public inline function set(x:Float, y:Float) {
-		this.x = x;
-		this.y = y;
+	public inline function setXY(vx:Float, vy:Float) {
+		setX(vx);
+		setY(vy);
 	}
 
 	/** Set both X and Y to the same value **/
-	public inline function setBoth(v:Float) {
-		return x = y = v;
+	public inline function set(v:Float) {
+		return _preMultX = _preMultY = v*preMul;
 	}
+	@:deprecated("Use set(f)") @:noCompletion public inline function setBoth(f:Float) set(f);
 
 	/** Add a vector to this one **/
 	public inline function addAng(ang:Float, v:Float) {
-		x += Math.cos(ang)*v;
-		y += Math.sin(ang)*v;
+		addX( Math.cos(ang) * v );
+		addY( Math.sin(ang) * v );
 	}
 
 	/** Set X and Y to specific values based on given angle and current length **/
 	public inline function setAng(ang:Float, v:Float) {
-		x = Math.cos(ang)*v;
-		y = Math.sin(ang)*v;
+		setX( Math.cos(ang) * v );
+		setY( Math.sin(ang) * v );
 	}
 
 	public inline function rotate(angInc:Float) {
 		var oldAng = ang;
 		var d = len;
-		dx = Math.cos(oldAng+angInc) * d;
-		dy = Math.sin(oldAng+angInc) * d;
+		_preMultX = Math.cos(oldAng+angInc) * d;
+		_preMultY = Math.sin(oldAng+angInc) * d;
 	}
 
 
 	/** Return true if X and Y are both below the `clearThreshold` value **/
-	public inline function isZero() return M.fabs(x)<=clearThreshold  &&  M.fabs(y)<=clearThreshold;
+	public inline function isZero() return M.fabs(_preMultX)<=clearThreshold  &&  M.fabs(_preMultY)<=clearThreshold;
 
 	/** Call this method to update Velocity at fixed/constant FPS **/
 	public inline function fixedUpdate(frictOverride=-1.) {
@@ -162,19 +184,19 @@ class Velocity {
 	/** Call this method to update Velocity at variable FPS **/
 	public inline function frameUpdate(tmod:Float, frictOverride=-1.) {
 		if( frictOverride>=0 ) {
-			x *= Math.pow(frictOverride,tmod);
-			y *= Math.pow(frictOverride,tmod);
+			_preMultX *= Math.pow(frictOverride,tmod);
+			_preMultY *= Math.pow(frictOverride,tmod);
 		}
 		else {
-			x *= Math.pow(frictX,tmod);
-			y *= Math.pow(frictY,tmod);
+			_preMultX *= Math.pow(frictX,tmod);
+			_preMultY *= Math.pow(frictY,tmod);
 		}
 
-		if( M.fabs(x)<clearThreshold )
-			x = 0;
+		if( M.fabs(_preMultX)<clearThreshold )
+			_preMultX = 0;
 
-		if( M.fabs(y)<clearThreshold )
-			y = 0;
+		if( M.fabs(_preMultY)<clearThreshold )
+			_preMultY = 0;
 	}
 
 
@@ -187,7 +209,7 @@ class Velocity {
 		CiAssert.equals( v.isZero(), true );
 		CiAssert.equals( v.shortString(), "0,0" );
 
-		v.set(8,2);
+		v.setXY(8,2);
 		CiAssert.equals( v.x, 8 );
 		CiAssert.equals( v.y, 2 );
 		CiAssert.equals( v.shortString(), "8,2" );
@@ -210,7 +232,7 @@ class Velocity {
 
 		// Different frictions
 		var v = new Velocity();
-		v.set(8,2);
+		v.setXY(8,2);
 		v.setFricts(0.25, 0.5);
 		v.fixedUpdate();
 		CiAssert.equals( v.shortString(), "2,1" );
@@ -219,7 +241,7 @@ class Velocity {
 
 
 		// Multiply
-		v.set(8,2);
+		v.setXY(8,2);
 		v.mul(2);
 		CiAssert.equals( v.shortString(), "16,4" );
 
@@ -231,32 +253,73 @@ class Velocity {
 		CiAssert.equals( v.isZero(), true );
 
 		// Addition
-		v.set(1,1);
+		v.setXY(1,1);
 		v.addXY(1,1);
 		CiAssert.equals( v.shortString(), "2,2" );
 
 		v.addXY(1,2);
 		CiAssert.equals( v.shortString(), "3,4" );
 
-		// Single value velocity
-		var v = new Velocity();
-		v.frict = 0.5;
-		v.v = 4;
-		CiAssert.equals( v.v, 4 );
-		v.fixedUpdate();
-		CiAssert.equals( v.v, 2 );
-		v.fixedUpdate();
-		CiAssert.equals( v.v, 1 );
-
 		// Frame updates
 		var v = new Velocity();
 		v.frict = 0.5;
-		v.v = 4;
-		CiAssert.equals( v.v, 4 );
+		v.setX(4);
+		CiAssert.equals( v.x, 4 );
 		v.frameUpdate(2);
-		CiAssert.equals( v.v, 1 );
+		CiAssert.equals( v.x, 1 );
 		v.frameUpdate(1);
-		CiAssert.equals( v.v, 0.5 );
+		CiAssert.equals( v.x, 0.5 );
+
+
+		// Set signs
+		var v = new Velocity();
+		v.set(5);
+		CiAssert.equals( v.x, 5 );
+		CiAssert.equals( v.y, 5 );
+		v.setSignX(-1);
+		CiAssert.equals( v.x, -5 );
+		v.setSignX(1);
+		CiAssert.equals( v.x, 5 );
+		v.setSignY(1);
+		CiAssert.equals( v.y, 5 );
+
+
+		// Pre-multiplied values
+		var v = new Velocity();
+		v.frict = 1;
+		v.preMul = 0.5;
+		CiAssert.equals( v.x, 0 );
+		CiAssert.equals( v.y, 0 );
+
+		v.setX(1);
+		v.setY(1);
+		CiAssert.equals( v.x, 0.5 );
+		CiAssert.equals( v.y, 0.5 );
+
+		v.setXY(1,1);
+		CiAssert.equals( v.x, 0.5 );
+		CiAssert.equals( v.y, 0.5 );
+
+		v.setAng(0, 1);
+		CiAssert.equals( v.x, 0.5 );
+		CiAssert.equals( v.y, 0 );
+
+		v.setX(100);
+		CiAssert.equals( v.x, 50 );
+		v.addX(100);
+		CiAssert.equals( v.x, 100 );
+
+		// Frame updates with pre-multiplied values
+		var v = new Velocity();
+		v.setXY(100,100);
+		v.preMul = 0.5;
+		v.frict = 0.25;
+		v.frameUpdate(1);
+		CiAssert.equals( v.x, 25 );
+		CiAssert.equals( v.y, 25 );
+		v.frameUpdate(1);
+		CiAssert.equals( v.x, 6.25 );
+		CiAssert.equals( v.y, 6.25 );
 	}
 	#end
 }
@@ -299,7 +362,7 @@ class VelocityArray {
 	public inline function getSumX() {
 		_sum = 0.;
 		for(v in all)
-			_sum+=v.x;
+			_sum+=v._preMultX;
 		return _sum;
 	}
 
@@ -307,7 +370,7 @@ class VelocityArray {
 	public inline function getSumY() {
 		_sum = 0.;
 		for(v in all)
-			_sum+=v.y;
+			_sum+=v._preMultY;
 		return _sum;
 	}
 
