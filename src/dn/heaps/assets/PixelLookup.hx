78ccd083d5f6c9cc5b8cb8c836b74070a85ad1d6
Sebastien Benard <github@deepnight.net>
2025-08-27 15:38:11 +0200

    ADD replaceColors

--- src/dn/heaps/assets/PixelLookup.hx
+++ src/dn/heaps/assets/PixelLookup.hx
@@ -16,19 +16,35 @@ class PixelLookup extends dn.Process {
 		return points;
 	}
 
+
 	public static function replaceColor(slib:dn.heaps.slib.SpriteLib, lookupColor:dn.Col, newColor:dn.Col) {
 		var pixels = slib.tile.getTexture().capturePixels();
 
-		lookupColor = lookupColor.withAlphaIfMissing();
 		for(x in 0...pixels.width)
 		for(y in 0...pixels.height) {
 			var c : Col = pixels.getPixel(x,y);
-			if( c==lookupColor )
+			if( c.withoutAlpha()==lookupColor )
 				pixels.setPixel(x,y, newColor.withAlphaIfMissing(c.af));
 		}
 		var newTile = h2d.Tile.fromPixels(pixels);
 		slib.tile.switchTexture(newTile);
 	}
+
+
+	public static function replaceColors(slib:dn.heaps.slib.SpriteLib, remap:Map<dn.Col, dn.Col>) {
+		var pixels = slib.tile.getTexture().capturePixels();
+
+		var c : Col = 0;
+		for(x in 0...pixels.width)
+		for(y in 0...pixels.height) {
+			c = pixels.getPixel(x,y);
+			if( remap.exists(c.withoutAlpha()) )
+				pixels.setPixel( x,y, remap.get( c.withoutAlpha() ).withAlpha(c.af) );
+		}
+		var newTile = h2d.Tile.fromPixels(pixels);
+		slib.tile.switchTexture(newTile);
+	}
+
 
 	static function lookupSubPixels(pixels:hxd.Pixels, x:Int, y:Int, w:Int, h:Int, col:Col) : Null<PixelPoint> {
 		for(px in x...x+w)
